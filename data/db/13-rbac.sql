ec58dd3290b79a9b3158c84341b6b6d47ecf6011
Christophe Robert <buse974@localhost.localdomain>
2015-05-04 15:45:47 +0200

    add right

--- data/db/13-rbac.sql
+++ data/db/13-rbac.sql
@@ -3,6 +3,14 @@ INSERT INTO `role_relation` (`role_id`, `parent_id`) VALUES ('3', '2');
 INSERT INTO `role_relation` (`role_id`, `parent_id`) VALUES ('5', '3');
 INSERT INTO `role_relation` (`role_id`, `parent_id`) VALUES ('4', '5');
 
+/*
+INSERT INTO `role` (`id`, `name`) VALUES ('1', 'super-admin');
+INSERT INTO `role` (`id`, `name`) VALUES ('2', 'admin');
+INSERT INTO `role` (`id`, `name`) VALUES ('3', 'academic');
+INSERT INTO `role` (`id`, `name`) VALUES ('4', 'student');
+INSERT INTO `role` (`id`, `name`) VALUES ('5', 'instructor');
+INSERT INTO `role` (`id`, `name`) VALUES ('6', 'recruiter');
+*/
 
 INSERT INTO `permission` (`libelle`) VALUES ('user.get');
 SELECT LAST_INSERT_ID() INTO @rbac_permission_id;
@@ -42,8 +50,8 @@ INSERT INTO `role_permission` (`role_id`, `permission_id`) VALUES (3, @rbac_perm
 
 INSERT INTO `permission` (`libelle`) VALUES ('user.logout');
 SELECT LAST_INSERT_ID() INTO @rbac_permission_id;
-INSERT INTO `role_permission` (`role_id`, `permission_id`) VALUES (1, @rbac_permission_id);
+INSERT INTO `role_permission` (`role_id`, `permission_id`) VALUES (4, @rbac_permission_id);
 
 INSERT INTO `permission` (`libelle`) VALUES ('program.get');
 SELECT LAST_INSERT_ID() INTO @rbac_permission_id;
-INSERT INTO `role_permission` (`role_id`, `permission_id`) VALUES (1, @rbac_permission_id);
+INSERT INTO `role_permission` (`role_id`, `permission_id`) VALUES (3, @rbac_permission_id);
