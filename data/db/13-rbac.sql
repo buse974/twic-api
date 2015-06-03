fc98720433dfa1f32b9a882685d1d9bc0ca16b0b
Christophe Robert <crobert@thestudnet.com>
2015-06-03 11:03:34 +0200

    rbac course.getList

--- data/db/13-rbac.sql
+++ data/db/13-rbac.sql
@@ -99,3 +99,7 @@ INSERT INTO `role_permission` (`role_id`, `permission_id`) VALUES (3, @rbac_perm
 INSERT INTO `permission` (`libelle`) VALUES ('course.add');
 SELECT LAST_INSERT_ID() INTO @rbac_permission_id;
 INSERT INTO `role_permission` (`role_id`, `permission_id`) VALUES (3, @rbac_permission_id);
+
+INSERT INTO `permission` (`libelle`) VALUES ('course.getList');
+SELECT LAST_INSERT_ID() INTO @rbac_permission_id;
+INSERT INTO `role_permission` (`role_id`, `permission_id`) VALUES (3, @rbac_permission_id);
