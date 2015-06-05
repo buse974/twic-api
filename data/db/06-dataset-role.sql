2cf01b11867448899d2597fed4e43f1ed9dbd14b
Christophe Robert <crobert@thestudnet.com>
2015-06-05 17:31:34 +0200

    superadmin

--- data/db/06-dataset-role.sql
+++ data/db/06-dataset-role.sql
@@ -1,4 +1,4 @@
-INSERT INTO `role` (`id`, `name`) VALUES ('1', 'super-admin');
+INSERT INTO `role` (`id`, `name`) VALUES ('1', 'super_admin');
 INSERT INTO `role` (`id`, `name`) VALUES ('2', 'admin');
 INSERT INTO `role` (`id`, `name`) VALUES ('3', 'academic');
 INSERT INTO `role` (`id`, `name`) VALUES ('4', 'student');
