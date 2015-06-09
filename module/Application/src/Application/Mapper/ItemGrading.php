8e9387249d44ddcc779c12499c2e807a0427cf0b
Christophe Robert <christophe.robert@babblecar.com>
2015-06-09 14:00:05 +0200

    update grading

--- module/Application/src/Application/Mapper/ItemGrading.php
+++ module/Application/src/Application/Mapper/ItemGrading.php
@@ -6,4 +6,12 @@ use Dal\Mapper\AbstractMapper;
 
 class ItemGrading extends AbstractMapper
 {
+	public function getList()
+	{
+		$select = $this->tableGateway->getSql()->select();
+		
+		$select->columns(array('grade', 'created_date'))
+			   ->join('grading_policy', 'item_grading.grading_policy_id=grading_policy.id');
+			   
+	}
 }
