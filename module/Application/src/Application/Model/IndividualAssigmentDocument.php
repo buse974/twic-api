14003afd7988eb3b2e4d670f8d0291eba0b37ad6
Christophe Robert <christophe.robert@babblecar.com>
2015-06-09 15:35:32 +0200

    item.add + clean code

--- module/Application/src/Application/Model/IndividualAssigmentDocument.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Application\Model;
-
-use Application\Model\Base\IndividualAssigmentDocument as BaseIndividualAssigmentDocument;
-
-class IndividualAssigmentDocument extends BaseIndividualAssigmentDocument
-{
-}
