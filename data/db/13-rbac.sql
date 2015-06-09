5b8482da050ebd23841d318d3e385c5c4e1b7b11
Christophe Robert <christophe.robert@babblecar.com>
2015-06-09 14:30:43 +0200

    grading_policy.update right

--- data/db/13-rbac.sql
+++ data/db/13-rbac.sql
@@ -107,3 +107,7 @@ INSERT INTO `role_permission` (`role_id`, `permission_id`) VALUES (3, @rbac_perm
 INSERT INTO `permission` (`libelle`) VALUES ('module.add');
 SELECT LAST_INSERT_ID() INTO @rbac_permission_id;
 INSERT INTO `role_permission` (`role_id`, `permission_id`) VALUES (3, @rbac_permission_id);
+
+INSERT INTO `permission` (`libelle`) VALUES ('grading_policy.update');
+SELECT LAST_INSERT_ID() INTO @rbac_permission_id;
+INSERT INTO `role_permission` (`role_id`, `permission_id`) VALUES (3, @rbac_permission_id);
