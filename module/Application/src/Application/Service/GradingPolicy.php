56c24e4ef73eb99011538f42187e43c1fb2d7a96
Christophe Robert <crobert@thestudnet.com>
2015-06-09 14:00:05 +0200

    update grading

--- module/Application/src/Application/Service/GradingPolicy.php
+++ module/Application/src/Application/Service/GradingPolicy.php
@@ -26,16 +26,34 @@ class GradingPolicy extends AbstractService
         return true;
     }
 
+
     /**
      * update grading policy.
-     *
      * @invokable
+     * @param array $datas
+     */
+    public function update($datas)
+    {
+    	$ret = array();
+    	foreach ($datas as $gp) {
+    		
+    		$name = isset($gp['name']) ? $gp['name']:null;
+    		$grade = isset($gp['grade']) ? $gp['grade']:null;
+    		
+    		$ret[$gp['id']] = $this->_update($gp['id'], $name, $grade);
+    	}
+    	
+    	return $ret;
+    }
+    
+    
+    /**
      *
      * @param int    $id
      * @param string $name
      * @param int    $grade
      */
-    public function update($id, $name = null, $grade = null)
+    public function _update($id, $name = null, $grade = null)
     {
         $m_grading = $this->getModel()
                           ->setName($name)
