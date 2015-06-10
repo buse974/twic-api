a610cc24f3bc365586f97ceeb7917c7c02931346
Christophe Robert <crobert@thestudnet.com>
2015-06-10 15:13:36 +0200

    item.getList

--- data/db/13-rbac.sql
+++ data/db/13-rbac.sql
@@ -119,3 +119,7 @@ INSERT INTO `role_permission` (`role_id`, `permission_id`) VALUES (3, @rbac_perm
 INSERT INTO `permission` (`libelle`) VALUES ('item.update');
 SELECT LAST_INSERT_ID() INTO @rbac_permission_id;
 INSERT INTO `role_permission` (`role_id`, `permission_id`) VALUES (3, @rbac_permission_id);
+
+INSERT INTO `permission` (`libelle`) VALUES ('item.getList');
+SELECT LAST_INSERT_ID() INTO @rbac_permission_id;
+INSERT INTO `role_permission` (`role_id`, `permission_id`) VALUES (3, @rbac_permission_id);
