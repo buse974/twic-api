37c68f89ec687460e09c8d8490c52ec5ea3158ab
Christophe Robert <christophe.robert@babblecar.com>
2015-06-11 12:10:58 +0200

    add relation

--- data/db/13-rbac.sql
+++ data/db/13-rbac.sql
@@ -1,3 +1,4 @@
+INSERT INTO `role_relation` (`role_id`, `parent_id`) VALUES ('1', '0');
 INSERT INTO `role_relation` (`role_id`, `parent_id`) VALUES ('2', '1');
 INSERT INTO `role_relation` (`role_id`, `parent_id`) VALUES ('3', '2');
 INSERT INTO `role_relation` (`role_id`, `parent_id`) VALUES ('5', '3');
