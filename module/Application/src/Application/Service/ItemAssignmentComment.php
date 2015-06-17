b0796357fe694bca5ffb9b106ec89e64e2227b8e
Christophe Robert <crobert@thestudnet.com>
2015-06-17 14:22:13 +0200

    item delete ok

--- module/Application/src/Application/Service/ItemAssignmentComment.php
+++ module/Application/src/Application/Service/ItemAssignmentComment.php
@@ -6,4 +6,22 @@ use Dal\Service\AbstractService;
 
 class ItemAssignmentComment extends AbstractService
 {
+	public function add($item_assignment_id, $text)
+	{
+		$m_item_assignment_comment = $this->getModel()->setItemAssignmentId($item_assignment_id)
+		                                              ->setText($text)
+		                                              ->setUserId($this->getServiceAuth()->getIdentity()->getId());
+		
+		
+		$this->getMapper()->insert($m_item_assignment_comment);
+		                
+	}
+	
+	/**
+	 * @return \Zend\Authentication\AuthenticationService
+	 */
+	public function getServiceAuth()
+	{
+		return $this->getServiceLocator()->get('auth.service');
+	}
 }
