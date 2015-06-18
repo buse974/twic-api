813a2aadf8e0ddc31af0ca9f2ef85b74bcd8843d
Christophe Robert <crobert@thestudnet.com>
2015-06-18 09:36:59 +0200

    right

--- data/db/13-rbac.sql
+++ data/db/13-rbac.sql
@@ -226,3 +226,11 @@ INSERT INTO `role_permission` (`role_id`, `permission_id`) VALUES (3, @rbac_perm
 INSERT INTO `permission` (`libelle`) VALUES ('materialdocument.update');
 SELECT LAST_INSERT_ID() INTO @rbac_permission_id;
 INSERT INTO `role_permission` (`role_id`, `permission_id`) VALUES (3, @rbac_permission_id);
+
+INSERT INTO `permission` (`libelle`) VALUES ('item.getListGrade');
+SELECT LAST_INSERT_ID() INTO @rbac_permission_id;
+INSERT INTO `role_permission` (`role_id`, `permission_id`) VALUES (3, @rbac_permission_id);
+
+INSERT INTO `permission` (`libelle`) VALUES ('itemassignment.addComment');
+SELECT LAST_INSERT_ID() INTO @rbac_permission_id;
+INSERT INTO `role_permission` (`role_id`, `permission_id`) VALUES (3, @rbac_permission_id);
