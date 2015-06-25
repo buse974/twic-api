4394f1d4479a79fc7589957ff44b58619339a683
Christophe Robert <crobert@thestudnet.com>
2015-06-25 18:27:35 +0200

    item_assignment_comment

--- module/Application/src/Application/Mapper/ItemAssignmentComment.php
+++ module/Application/src/Application/Mapper/ItemAssignmentComment.php
@@ -6,4 +6,17 @@ use Dal\Mapper\AbstractMapper;
 
 class ItemAssignmentComment extends AbstractMapper
 {
+	public function getList($item, $user)
+	{
+		$select = $this->tableGateway->getSql()->select();
+		
+		$select->columns(array('id', 'text', 'user_id', 'item_assignment_id', 'created_date', 'read_date'))
+		       ->join('item_assignment', 'item_assignment.id=item_assignment_comment.item_assignment_id', array())
+		       ->join('item_assignment_user', 'item_assignment_user.item_assignment_id=item_assignment_comment.item_assignment_id', array())
+		       ->join('item_prog', 'item_assignment.item_prog_id=item_prog.id', array())
+			   ->where(array('item_prog.item_id' => $item))
+			   ->where(array('item_assignment_user.user_id' => $user));
+		
+		return $this->selectWith($select);
+	}
 }
