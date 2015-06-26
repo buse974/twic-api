74f8efceea55659df95fd8b03a66e79588cbefb7
Christophe Robert <crobert@thestudnet.com>
2015-06-26 09:51:25 +0200

    add avatar user

--- module/Application/src/Application/Mapper/GradingPolicyGradeComment.php
+++ module/Application/src/Application/Mapper/GradingPolicyGradeComment.php
@@ -11,7 +11,7 @@ class GradingPolicyGradeComment extends AbstractMapper
 		$select = $this->tableGateway->getSql()->select();
 		
 		$select->columns(array('id', 'text', 'user_id','created_date'))
-		       ->join('user', 'user.id=grading_policy_grade_comment.user_id', array('id', 'firstname', 'lastname'))
+		       ->join('user', 'user.id=grading_policy_grade_comment.user_id', array('id', 'firstname', 'lastname', 'avatar'))
 			   ->join('grading_policy_grade', 'grading_policy_grade.id=grading_policy_grade_comment.grading_policy_grade_id')
 			   ->where(array('grading_policy_grade.grading_policy_id' => $grading_policy))
 			   ->where(array('grading_policy_grade.user_id' => $user));
--- module/Application/src/Application/Mapper/ItemAssignmentComment.php
+++ module/Application/src/Application/Mapper/ItemAssignmentComment.php
@@ -11,7 +11,7 @@ class ItemAssignmentComment extends AbstractMapper
 		$select = $this->tableGateway->getSql()->select();
 		
 		$select->columns(array('id', 'text', 'item_assignment_id', 'created_date', 'read_date'))
-			   ->join('user', 'user.id=user_id', array('id', 'firstname', 'lastname'))
+			   ->join('user', 'user.id=user_id', array('id', 'firstname', 'lastname', 'avatar'))
 		       ->join('item_assignment', 'item_assignment.id=item_assignment_comment.item_assignment_id', array())
 		       ->join('item_assignment_user', 'item_assignment_user.item_assignment_id=item_assignment_comment.item_assignment_id', array())
 		       ->join('item_prog', 'item_assignment.item_prog_id=item_prog.id', array())
