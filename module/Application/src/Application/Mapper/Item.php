f5a44525f2e9924f168e5d643925d7806c7bc5da
Christophe Robert <christophe.robert@babblecar.com>
2015-07-08 16:05:00 +0200

    item.getListGrade

--- module/Application/src/Application/Mapper/Item.php
+++ module/Application/src/Application/Mapper/Item.php
@@ -17,23 +17,23 @@ class Item extends AbstractMapper
 
         $select_new_message = new Select('item_assignment_comment');
         $select_new_message->columns(array('rrr' => new Expression('COUNT(1)')))
-                           ->where(array('item_assignment_comment.item_assignment_id=item_assignment.id'))
+                           ->where(array('item_assignment_comment.item_assignment_id=item_item_prog_item_assignment.id'))
                            ->where(array('item_assignment_comment.read_date IS NULL'));
 
-        $select->columns(array('id', 'title', 'item$new_message' => $select_new_message, 'submit_date' => new Expression('DATE_FORMAT(submit_date, "%Y-%m-%dT%TZ")')))
+        $select->columns(array('id', 'title', 'item$new_message' => $select_new_message))
                ->join('module', 'module.id=item.module_id', array('id', 'title'), $select::JOIN_LEFT)
                ->join('course', 'course.id=item.course_id', array('id', 'title'))
                ->join('program', 'program.id=course.program_id', array('id', 'name'))
                ->join('item_prog', 'item_prog.item_id=item.id', array('id', 'item_prog$due_date' => new Expression('DATE_FORMAT(due_date, "%Y-%m-%dT%TZ")') ,'item_prog$start_date' => new Expression('DATE_FORMAT(start_date, "%Y-%m-%dT%TZ")')))
                ->join('item_prog_user', 'item_prog_user.item_prog_id = item_prog.id',array())
-               ->join('item_assignment', 'item_assignment.item_prog_id=item_prog.id', array('id', 'submit_date'), $select::JOIN_LEFT)
-               ->join('item_assignment_user', 'item_assignment_user.item_assignment_id=item_assignment.id AND item_assignment_user.user_id=item_prog_user.user_id', array(), $select::JOIN_LEFT)
-               ->join('item_grading', 'item_grading.item_prog_user_id=item_prog_user.id', array('grade'), $select::JOIN_LEFT)
-               ->join('grading', 'item_grading.grade BETWEEN grading.min AND grading.max', array('item_grading$letter' => 'letter'), $select::JOIN_LEFT)
+               ->join(array('item_item_prog_item_assignment' => 'item_assignment'), 'item_item_prog_item_assignment.item_prog_id=item_prog.id', array('id', 'item_item_prog_item_assignment$submit_date' => new Expression('DATE_FORMAT(submit_date, "%Y-%m-%dT%TZ")')), $select::JOIN_LEFT)
+               ->join('item_assignment_user', 'item_assignment_user.item_assignment_id=item_item_prog_item_assignment.id AND item_assignment_user.user_id=item_prog_user.user_id', array(), $select::JOIN_LEFT)
+               ->join(array('item_item_prog_item_grading' => 'item_grading'), 'item_item_prog_item_grading.item_prog_user_id=item_prog_user.id', array('grade'), $select::JOIN_LEFT)
+               ->join('grading', 'item_item_prog_item_grading.grade BETWEEN grading.min AND grading.max', array('item_item_prog_item_grading$letter' => 'letter'), $select::JOIN_LEFT)
                ->join('user', 'item_assignment_user.user_id=user.id', array(), $select::JOIN_LEFT)
                ->where(array('program.id' => $programs))
                ->where(array('item_prog.start_date < UTC_TIMESTAMP'))
-               ->order(array('item_assignment.submit_date' => 'DESC'))
+               ->order(array('item_item_prog_item_assignment.submit_date' => 'DESC'))
                ->quantifier('DISTINCT');
 
         
@@ -51,14 +51,14 @@ class Item extends AbstractMapper
         
         if($notgraded === true || $newMessage === true){
             if ($newMessage === true) {
-                $select->join('item_assignment_comment', 'item_assignment_comment.item_assignment_id=item_assignment.id', array(), $select::JOIN_LEFT)
+                $select->join('item_assignment_comment', 'item_assignment_comment.item_assignment_id=item_item_prog_item_assignment.id', array(), $select::JOIN_LEFT)
                        ->where(array('( ( item_assignment_comment.id IS NOT NULL AND item_assignment_comment.read_date IS NULL) '));
             }
             else{
                   $select->where(array('( 0'));
             }
             if ($notgraded === true) {
-                $select->where(array(' item_grading.id IS NULL )'), Predicate::OP_OR);
+                $select->where(array(' item_item_prog_item_grading.id IS NULL )'), Predicate::OP_OR);
             }
             else{
                 $select->where(array(' 0 )'), Predicate::OP_OR);
@@ -66,7 +66,7 @@ class Item extends AbstractMapper
         }
         
         if(!array_key_exists(ModelRole::ROLE_STUDENT_ID, $user['roles'])) {
-        	$select->where(array('item_assignment.submit_date IS NOT NULL'));
+        	$select->where(array('item_item_prog_item_assignment.submit_date IS NOT NULL'));
         } else {
         	$select->where(array('item_prog_user.user_id' => $user['id']));
         }
