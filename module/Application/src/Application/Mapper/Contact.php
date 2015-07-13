d6ee02cc33ad0d333419942e7ae0c00cf304d475
Christophe Robert <crobert@thestudnet.com>
2015-07-13 11:09:01 +0200

    contact

--- module/Application/src/Application/Mapper/Contact.php
+++ module/Application/src/Application/Mapper/Contact.php
@@ -1,9 +1,24 @@
 <?php
-
 namespace Application\Mapper;
 
 use Dal\Mapper\AbstractMapper;
 
 class Contact extends AbstractMapper
 {
+
+    /**
+     * 
+     * @return \Zend\Db\ResultSet\ResultSet
+     */
+    public function getList($user)
+    {
+        $select = $this->tableGateway->getSql()->select();
+        
+        $select->columns(array())
+            ->join('user', 'user.id=contact.contact_id', array('id','firstname','lastname','avatar'), $select::JOIN_INNER)
+            ->where(array('contact.user_id' => $user))
+            ->where(array('contact.accepted_date IS NOT NULL'));
+        
+        return $this->selectWith($select);
+    }
 }
--- module/Application/src/Application/Model/Contact.php
+++ module/Application/src/Application/Model/Contact.php
@@ -6,4 +6,26 @@ use Application\Model\Base\Contact as BaseContact;
 
 class Contact extends BaseContact
 {
+    protected $contact;
+    
+    public function exchangeArray(array &$data)
+    {
+        parent::exchangeArray($data);
+    
+        $this->contact = new User($this);
+        
+        $this->contact->exchangeArray($data);
+    }
+    
+    public function getContact() 
+    {
+        return $this->contact;
+    }
+     
+    public function setContact($contact) 
+    {
+        $this->contact = $contact;
+        
+        return $this;
+    }     
 }
