b6ebc57189cb83928dfe44f5efb0f1881bfae7b1
Christophe Robert <crobert@thestudnet.com>
2015-07-22 10:43:11 +0200

    contact delete

--- module/Application/src/Application/Service/Contact.php
+++ module/Application/src/Application/Service/Contact.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Application\Service;
 
 use Dal\Service\AbstractService;
@@ -7,17 +6,18 @@ use Zend\Db\Sql\Predicate\IsNotNull;
 
 class Contact extends AbstractService
 {
+
     /**
      * @invokable
      *
-     * @param int $user
+     * @param int $user            
      */
     public function add($user)
     {
         $identity = $this->getServiceUser()->getIdentity();
         
-        if($user == $identity['id']) {
-            throw new \Exception('error user equal myself');    
+        if ($user == $identity['id']) {
+            throw new \Exception('error user equal myself');
         }
         
         $m_contact = $this->getModel()
@@ -28,46 +28,54 @@ class Contact extends AbstractService
         $m_contact = $this->getModel()
             ->setUserId($user)
             ->setContactId($identity['id']);
-
+        
         return $this->getMapper()->insert($m_contact);
     }
 
     /**
      * @invokable
      *
-     * @param int $user
+     * @param int $user            
      */
     public function accept($user)
     {
         $identity = $this->getServiceUser()->getIdentity();
         $m_contact = $this->getModel()->setAcceptedDate((new \DateTime('now', new \DateTimeZone('UTC')))->format('Y-m-d H:i:s'));
-
-        if($this->getMapper()->update($m_contact, array('user_id' => $user, 'contact_id' => $identity['id'], new IsNotNull('request_date'))) <= 0) {
+        
+        if ($this->getMapper()->update($m_contact, array('user_id' => $user,'contact_id' => $identity['id'],new IsNotNull('request_date'))) <= 0) {
             throw new \Eception('Error accept user');
         }
         
-        return $this->getMapper()->update($m_contact, array('user_id' => $identity['id'], 'contact_id' => $user));
+        return $this->getMapper()->update($m_contact, array('user_id' => $identity['id'],'contact_id' => $user));
     }
 
     /**
      * @invokable
      *
-     * @param int $user
+     * @param int $user            
      */
     public function remove($user)
     {
         $identity = $this->getServiceUser()->getIdentity();
-        $m_contact = $this->getModel()->setDeletedDate((new \DateTime('now', new \DateTimeZone('UTC')))->format('Y-m-d H:i:s'));
-
-        $this->getMapper()->update($m_contact, array('user_id' => $user, 'contact_id' => $identity['id']));
-
-        return $this->getMapper()->update($m_contact, array('user_id' => $identity['id'], 'contact_id' => $user));
+        
+        // $m_contact = $this->getModel()->setDeletedDate((new \DateTime('now', new \DateTimeZone('UTC')))->format('Y-m-d H:i:s'));
+        
+        $m_contact = $this->getModel()
+            ->setUserId($identity['id'])
+            ->setContactId($user);
+        $this->getMapper()->delete($m_contact);
+        
+        $m_contact = $this->getModel()
+            ->setUserId($user)
+            ->setContactId($identity['id']);
+        
+        return $this->getMapper()->delete($m_contact);
     }
 
     /**
      * @invokable
      *
-     * @param int $school
+     * @param int $school            
      */
     public function addBySchool($school)
     {
@@ -77,12 +85,12 @@ class Contact extends AbstractService
     /**
      * @invokable
      *
-     * @param string $all
+     * @param string $all            
      */
     public function getListRequest($all = false)
     {
         $identity = $this->getServiceUser()->getIdentity();
-
+        
         return $this->getMapper()->getListRequest($identity['id']);
     }
 
@@ -92,11 +100,12 @@ class Contact extends AbstractService
     public function getList()
     {
         $identity = $this->getServiceUser()->getIdentity();
-
+        
         return $this->getMapper()->getList($identity['id']);
     }
 
     /**
+     *
      * @return \Application\Service\User
      */
     public function getServiceUser()
