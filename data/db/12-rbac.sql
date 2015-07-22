a28a0ed513092544dd843f2523ab51b52a40386d
Xuan <xhoang@thestudnet.com>
2015-07-22 11:04:24 +0200

    droit research

--- data/db/12-rbac.sql
+++ data/db/12-rbac.sql
@@ -371,3 +371,8 @@ INSERT INTO `role_permission` (`role_id`, `permission_id`) VALUES (5, @rbac_perm
 INSERT INTO `permission` (`libelle`) VALUES ('course.getListRecord');
 SELECT LAST_INSERT_ID() INTO @rbac_permission_id;
 INSERT INTO `role_permission` (`role_id`, `permission_id`) VALUES (4, @rbac_permission_id);
+
+INSERT INTO `permission` (`libelle`) VALUES ('research.getList');
+SELECT LAST_INSERT_ID() INTO @rbac_permission_id;
+INSERT INTO `role_permission` (`role_id`, `permission_id`) VALUES (4, @rbac_permission_id);
+
