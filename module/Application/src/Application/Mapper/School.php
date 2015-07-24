10ce5521e8d6de5e821e8b14c69a54d133c5562f
Arthur Flachs <aflachs@thestudnet.com>
2015-07-24 11:22:33 +0200

    This adds the addresses of the schools requested with school.getList

--- module/Application/src/Application/Mapper/School.php
+++ module/Application/src/Application/Mapper/School.php
@@ -39,6 +39,10 @@ class School extends AbstractMapper
     {
         $select = $this->tableGateway->getSql()->select();
         $select->columns(array('id', 'name', 'next_name', 'short_name', 'logo', 'describe', 'website', 'programme', 'backroung', 'phone'))
+               ->join(array('school_address' => 'address'), 'school.address_id = school_address.id', array('id', 'street_no', 'street_type', 'street_name', 'floor', 'door', 'apartment', 'building', 'longitude', 'latitude', 'timezone'),  $select::JOIN_LEFT)
+               ->join(array('school_address_division' => 'division'), 'school_address_division.id=school_address.division_id', array('id', 'name'),  $select::JOIN_LEFT)
+               ->join(array('school_address_city' => 'city'), 'school_address_city.id=school_address.city_id', array('id', 'name'),  $select::JOIN_LEFT)
+               ->join(array('school_address_country' => 'country'), 'school_address_country.id=school_address.country_id', array('id', 'short_name', 'name'),  $select::JOIN_LEFT)
                 ->where('school.deleted_date IS NULL');
         
         return $this->selectWith($select);
