db6afe68ce2ab9d31c6cd7e3276570e0ee74f9ac
Christophe Robert <crobert@thestudnet.com>
2015-08-10 10:56:40 +0200

    right ok

--- data/db/12-rbac.sql
+++ data/db/12-rbac.sql
@@ -498,3 +498,7 @@ INSERT INTO `role_permission` (`role_id`, `permission_id`) VALUES (4, @rbac_perm
 INSERT INTO `permission` (`libelle`) VALUES ('conversation.add');
 SELECT LAST_INSERT_ID() INTO @rbac_permission_id;
 INSERT INTO `role_permission` (`role_id`, `permission_id`) VALUES (4, @rbac_permission_id);
+
+INSERT INTO `permission` (`libelle`) VALUES ('message.sendMail');
+SELECT LAST_INSERT_ID() INTO @rbac_permission_id;
+INSERT INTO `role_permission` (`role_id`, `permission_id`) VALUES (4, @rbac_permission_id);
