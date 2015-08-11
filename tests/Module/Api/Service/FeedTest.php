01063577eb13b59c3368a6748ad748b8ae74a9d7
Christophe Robert <crobert@thestudnet.com>
2015-08-11 16:50:38 +0200

    #447 - [Conversation] conversation.unread
    Paramètre : conversation: [Array de conversations] #done

---
+++ tests/Module/Api/Service/FeedTest.php
@@ -0,0 +1,190 @@
+<?php
+namespace ModuleTest\Api\Service;
+
+use ModuleTest\Api\AbstractService;
+
+class FeedTest extends AbstractService
+{
+
+    public static $session;
+
+    public static function setUpBeforeClass()
+    {
+        system('phing -q reset-db deploy-db');
+        
+        parent::setUpBeforeClass();
+    }
+
+    public function testAdd()
+    {
+        $this->setIdentity(1);
+        
+        $data = $this->jsonRpc('feed.add', array('content' => 'UN FEED','link' => 'link','video' => 'token','picture' => 'tokenp','document' => 'tokend'));
+        
+        $this->assertEquals(count($data), 3);
+        $this->assertEquals($data['result'], 1);
+        $this->assertEquals($data['id'], 1);
+        $this->assertEquals($data['jsonrpc'], 2.0);
+        
+        return $data['result'];
+    }
+
+    public function testAddTwo()
+    {
+        $this->setIdentity(2);
+        
+        $data = $this->jsonRpc('feed.add', array('content' => 'UN FEED2','link' => 'link2','video' => 'token2','picture' => 'tokenp2','document' => 'tokend2'));
+        
+        $this->assertEquals(count($data), 3);
+        $this->assertEquals($data['result'], 2);
+        $this->assertEquals($data['id'], 1);
+        $this->assertEquals($data['jsonrpc'], 2.0);
+        
+        return $data['result'];
+    }
+
+    public function testgetList()
+    {
+        $this->setIdentity(1);
+        
+        $data = $this->jsonRpc('feed.getList', array());
+        
+        $this->assertEquals(count($data), 3);
+        $this->assertEquals(count($data['result']), 2);
+        $this->assertEquals(count($data['result'][0]), 9);
+        $this->assertEquals($data['result'][0]['id'], 1);
+        $this->assertEquals($data['result'][0]['content'], "UN FEED");
+        $this->assertEquals($data['result'][0]['user_id'], 1);
+        $this->assertEquals($data['result'][0]['link'], "link");
+        $this->assertEquals($data['result'][0]['video'], "token");
+        $this->assertEquals($data['result'][0]['picture'], "tokenp");
+        $this->assertEquals($data['result'][0]['document'], "tokend");
+        $this->assertEquals(! empty($data['result'][0]['created_date']), true);
+        $this->assertEquals($data['result'][0]['deleted_date'], null);
+        $this->assertEquals(count($data['result'][1]), 9);
+        $this->assertEquals($data['result'][1]['id'], 2);
+        $this->assertEquals($data['result'][1]['content'], "UN FEED2");
+        $this->assertEquals($data['result'][1]['user_id'], 2);
+        $this->assertEquals($data['result'][1]['link'], "link2");
+        $this->assertEquals($data['result'][1]['video'], "token2");
+        $this->assertEquals($data['result'][1]['picture'], "tokenp2");
+        $this->assertEquals($data['result'][1]['document'], "tokend2");
+        $this->assertEquals(! empty($data['result'][1]['created_date']), true);
+        $this->assertEquals($data['result'][1]['deleted_date'], null);
+        $this->assertEquals($data['id'], 1);
+        $this->assertEquals($data['jsonrpc'], 2.0);
+    }
+
+    /**
+     * @depends testAddTwo
+     */
+    public function testAddComment($feed)
+    {
+        $this->setIdentity(2);
+        
+        $data = $this->jsonRpc('feed.addComment', array('content' => 'UN FEED2','id' => $feed));
+        
+        $this->assertEquals(count($data), 3);
+        $this->assertEquals($data['result'], 1);
+        $this->assertEquals($data['id'], 1);
+        $this->assertEquals($data['jsonrpc'], 2.0);
+        
+        return $data['result'];
+    }
+
+    /**
+     * @depends testAddTwo
+     */
+    public function testgetListComment($feed)
+    {
+        $this->setIdentity(2);
+        
+        $data = $this->jsonRpc('feed.GetListComment', array('id' => $feed));
+        
+        $this->assertEquals(count($data) , 3);
+        $this->assertEquals(count($data['result']) , 1);
+        $this->assertEquals(count($data['result'][0]) , 6);
+        $this->assertEquals($data['result'][0]['id'] , 1);
+        $this->assertEquals($data['result'][0]['content'] , "UN FEED2");
+        $this->assertEquals($data['result'][0]['user_id'] , 2);
+        $this->assertEquals($data['result'][0]['feed_id'] , 2);
+        $this->assertEquals(!empty($data['result'][0]['created_date']) , true);
+        $this->assertEquals($data['result'][0]['deleted_date'] , null);
+        $this->assertEquals($data['id'] , 1);
+        $this->assertEquals($data['jsonrpc'] , 2.0);
+    }
+    
+    // DELETE 
+    
+    /**
+     * @depends testAddComment
+     */
+    public function testdeleteComment($feed_comment)
+    {
+        $this->setIdentity(2);
+    
+        $data = $this->jsonRpc('feed.deleteComment', array('id' => $feed_comment));
+        
+        $this->assertEquals(count($data) , 3); 
+        $this->assertEquals($data['result'] , 1); 
+        $this->assertEquals($data['id'] , 1); 
+        $this->assertEquals($data['jsonrpc'] , 2.0);
+        
+    }
+    
+    /**
+     * @depends testAdd
+     */
+    public function testdelete($feed)
+    {
+        $this->setIdentity(1);
+    
+        $data = $this->jsonRpc('feed.delete', array('id' => $feed));
+    
+        $this->assertEquals(count($data) , 3);
+        $this->assertEquals($data['result'] , 1);
+        $this->assertEquals($data['id'] , 1);
+        $this->assertEquals($data['jsonrpc'] , 2.0);
+    
+    }
+
+    public function setIdentity($id)
+    {
+        $identityMock = $this->getMockBuilder('\Auth\Authentication\Adapter\Model\Identity')
+            ->disableOriginalConstructor()
+            ->getMock();
+        
+        $rbacMock = $this->getMockBuilder('\Rbac\Service\Rbac')
+            ->disableOriginalConstructor()
+            ->getMock();
+        
+        $identityMock->expects($this->any())
+            ->method('getId')
+            ->will($this->returnValue($id));
+        
+        $identityMock->expects($this->any())
+            ->method('toArray')
+            ->will($this->returnValue(array('id' => $id)));
+        
+        $authMock = $this->getMockBuilder('\Zend\Authentication\AuthenticationService')
+            ->disableOriginalConstructor()
+            ->getMock();
+        
+        $authMock->expects($this->any())
+            ->method('getIdentity')
+            ->will($this->returnValue($identityMock));
+        
+        $authMock->expects($this->any())
+            ->method('hasIdentity')
+            ->will($this->returnValue(true));
+        
+        $rbacMock->expects($this->any())
+            ->method('isGranted')
+            ->will($this->returnValue(true));
+        
+        $serviceManager = $this->getApplicationServiceLocator();
+        $serviceManager->setAllowOverride(true);
+        $serviceManager->setService('auth.service', $authMock);
+        $serviceManager->setService('rbac.service', $rbacMock);
+    }
+}
