65a2b5851b33bfaa582d52100a246b379697a630
Christophe Robert <crobert@thestudnet.com>
2015-08-11 09:55:15 +0200

    bug

--- module/Application/src/Application/Service/ItemAssignment.php
+++ module/Application/src/Application/Service/ItemAssignment.php
@@ -106,6 +106,8 @@ class ItemAssignment extends AbstractService
         
         $datetime1 = new \DateTime($m_item_prog->getStartDate());
         $datetime2 = new \DateTime('now', new DateTimeZone('UTC'));
+        
+        echo "\n" . $datetime1 . " >  ".  $datetime2 . "\n\n";
         if ($datetime1 > $datetime2) {
             throw new \Exception('error date');
         }
