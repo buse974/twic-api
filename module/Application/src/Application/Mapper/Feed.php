cced443e9e6ef303e8f8890a792d1cc36702f642
Christophe Robert <christophe.robert@babblecar.com>
2015-08-13 10:17:51 +0200

    #460 - feed.getList
    created_date : Format iso #done

--- module/Application/src/Application/Mapper/Feed.php
+++ module/Application/src/Application/Mapper/Feed.php
@@ -2,13 +2,17 @@
 namespace Application\Mapper;
 
 use Dal\Mapper\AbstractMapper;
+use Zend\Db\Sql\Predicate\Expression;
+
 
 class Feed extends AbstractMapper
 {
     public function getList($user)
     {
         $select = $this->tableGateway->getSql()->select();
-        $select->columns(array('id','content','user_id','link','video','picture','document','created_date'))
+        $select->columns(array('id','content','user_id','link','video','picture','document',
+            'feed$created_date' => new Expression('DATE_FORMAT(created_date, "%Y-%m-%dT%TZ")')
+        ))
             ->join('user', 'user.id=feed.user_id', array('id','firstname','lastname','avatar'))
             ->join('school', 'school.id=user.school_id', array('id','name','short_name','logo'), $select::JOIN_LEFT)
             ->where(array('feed.deleted_date IS NULL'))
