611489c1ab8bd4f1efe01b6d1fe4f5d1723751c5
Christophe Robert <crobert@thestudnet.com>
2015-08-17 14:33:55 +0200

    feed getList

--- module/Application/src/Application/Mapper/Feed.php
+++ module/Application/src/Application/Mapper/Feed.php
@@ -14,13 +14,14 @@ class Feed extends AbstractMapper
             'feed$created_date' => new Expression('DATE_FORMAT(feed.created_date, "%Y-%m-%dT%TZ")')
         ))
             ->join('user', 'user.id=feed.user_id', array('id','firstname','lastname','avatar'))
-            ->join('like', 'feed.id=like.feed_id', array('feed$nb_like' => new Expression('COUNT(like.feed_id)')), $select::JOIN_LEFT)
-            ->join(array('melike' => 'like'), 'feed.id=melike.feed_id', array('feed$is_like' => new Expression('MAX(IF(melike.user_id = '.$me.', 1, 0))')), $select::JOIN_LEFT)
+            ->join('like', 'feed.id=like.feed_id', array('feed$nb_like' => new Expression('COUNT(like.feed_id)'),
+                'feed$is_like' => new Expression('MAX(IF(like.user_id = '.$me.', 1, 0))')), $select::JOIN_LEFT)
             ->join('school', 'school.id=user.school_id', array('id','name','short_name','logo'), $select::JOIN_LEFT)
             ->where(array('feed.deleted_date IS NULL'))
             ->where(array('feed.user_id' => $user))
             ->group('feed.id')
             ->order(array('feed.id DESC'));
+        
         return $this->selectWith($select);
     }
 }
