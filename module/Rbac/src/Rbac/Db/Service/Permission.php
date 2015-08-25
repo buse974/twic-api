11e82920e922f4798816bbd2a570059e45609536
Christophe Robert <crobert@thestudnet.com>
2015-08-25 14:25:23 +0200

    #478 - memcached rbac
    rbac.add, update, delete  redémarrage du deuxieme memcached #done

--- module/Rbac/src/Rbac/Db/Service/Permission.php
+++ module/Rbac/src/Rbac/Db/Service/Permission.php
@@ -1,93 +1,101 @@
 <?php
-
 namespace Rbac\Db\Service;
 
 use Dal\Service\AbstractService;
 
 class Permission extends AbstractService
 {
+
     public function getPermission()
     {
         return $this->getMapper()->getPermissions();
     }
 
     public function getPermissionByRole($role)
-    {
-    }
+    {}
 
     public function insert($perm)
     {
         $this->getMapper()->insert($perm);
-
+        
         return $this->getMapper()->getLastInsertValue();
     }
-    
+
     /**
-     * Add permission     
+     * Add permission
      * @invokable
      *
-     * @param string $permission
+     * @param string $permission            
      *
      * @return int
      */
     public function add($permission)
     {
         $m_permission = $this->getModel();
-
+        
         $m_permission->setLibelle($permission);
-
+        
         if ($this->getMapper()->insert($m_permission) <= 0) {
             throw new \Exception('error insert');
         }
-
+        
+        $this->getServiceRbac()->createRbac();
         return $this->getMapper()->getLastInsertValue();
     }
-    
+
     /**
      * Delete Permission by libelle
      * @invokable
-     *  
-     * @param string $libelle
+     *
+     * @param string $libelle            
      *
      * @return int
      */
     public function delete($libelle)
     {
         $ret = array();
-        if (!is_array($libelle)) {
+        if (! is_array($libelle)) {
             $libelle = array($libelle);
         }
-
+        
         foreach ($libelle as $i) {
             $m_libelle = $this->getModel();
             $m_libelle->setLibelle($i);
             $ret[$i] = $this->getMapper()->delete($m_libelle);
         }
-
+        
+        $this->getServiceRbac()->createRbac();
+        
         return $ret;
-    }     
-    
+    }
+
     /**
      * @invokable
      */
     public function update($mPermission)
     {
-        return $this->getMapper()->update($mPermission);
+        $ret = $this->getMapper()->update($mPermission);
+        
+        if($ret > 0) {
+            $this->getServiceRbac()->createRbac();
+        }
+        
+        return $ret;
     }
 
     public function getListByRole($role)
     {
         return $this->getMapper()->getListByRole($role);
     }
-    
+
     /**
      * Get permission list.
      *
-     * @param string $filter
-     * @param string $search
-     * @param int $roleId
-     * 
      * @invokable
+     *
+     * @param string $filter            
+     * @param string $search            
+     * @param int $roleId            
      *
      * @return array
      */
@@ -95,8 +103,15 @@ class Permission extends AbstractService
     {
         $mapper = $this->getMapper();
         $res = $mapper->usePaginator($filter)->getList($filter, $roleId, $search);
-
-        return array('list' => $res,
-                    'count' => $mapper->count());
+        
+        return array('list' => $res,'count' => $mapper->count());
+    }
+    
+    /**
+     * @return \Rbac\Service\Rbac
+     */
+    public function getServiceRbac()
+    {
+        return $this->getServiceLocator()->get('rbac.service');
     }
 }
