791483cf60a04a603ae6cabb329cf24c6122b0ce
Xuan <xhoang@thestudnet.com>
2015-08-25 15:51:54 +0200

    address + error background libelle

--- data/db/12-rbac.sql
+++ data/db/12-rbac.sql
@@ -372,11 +372,11 @@ INSERT INTO `role_permission` (`role_id`, `permission_id`) VALUES (4, @rbac_perm
 INSERT INTO `permission` (`libelle`) VALUES ('school.getList');
 SELECT LAST_INSERT_ID() INTO @rbac_permission_id;
 INSERT INTO `role_permission` (`role_id`, `permission_id`) VALUES (4, @rbac_permission_id);
-
+/*
 INSERT INTO `permission` (`libelle`) VALUES ('school.add');
 SELECT LAST_INSERT_ID() INTO @rbac_permission_id;
 INSERT INTO `role_permission` (`role_id`, `permission_id`) VALUES (1, @rbac_permission_id);
-
+*/
 INSERT INTO `permission` (`libelle`) VALUES ('school.update');
 SELECT LAST_INSERT_ID() INTO @rbac_permission_id;
 INSERT INTO `role_permission` (`role_id`, `permission_id`) VALUES (1, @rbac_permission_id);
