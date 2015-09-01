642450a485aabbf92e72e4efdca6b74ac4672a84
Christophe Robert <christophe.robert@babblecar.com>
2015-09-01 15:22:14 +0200

    zuploadopentok

--- bin/uploadZopentok.sh
+++ bin/uploadZopentok.sh
@@ -25,7 +25,6 @@ if [ -z "$P" ] || [ -z "$URL_API" ] || [ ! -z "$H" ] ; then
  	echo -e "\n\n-----------------------------------------------------------------------------------";
 	echo -e "| uploadZopentok.sh -a <url_api> -u <url_destination> -p <path_upload_file> -h    |";
         echo -e "| -a <url_api> : Url api                                                          |";
-        echo -e "| -u <url_destination> : Url destination for download file                        |";
         echo -e "| -p <path_upload_file> : Path write file                                         |";
         echo -e "| -x <proxyhost[:port]> : Proxy                                                   |";
         echo -e "| -h : help                                                                       |";
@@ -46,15 +45,15 @@ if [ ! $LINE = "[]" ]; then
         OUT=`curl $X -w '%{http_code}' $(echo $LINE | jq -r '.url') -o $PATH_DEST`
         if [[ $OUT -eq 200 ]]
         then
-                RES=`curl -s --data-binary  "{\"jsonrpc\": \"2.0\", \"id\":1, \"method\": \"videoconf.validTransfertVideo\", \"params\": {\"videoconf_archive\": $(echo $LINE | jq -r '.id'),\"url\":\""$NAME"\"  } }" $URL_API`
+                RES=`curl -s --data-binary  "{\"jsonrpc\": \"2.0\", \"id\":1, \"method\": \"videoconf.validTransfertVideo\", \"params\": {\"videoconf_archive\": $(echo $LINE | jq -r '.id'),\"url\":\""$NAMEBASE"\"  } }" $URL_API`
                 echo "Download ok : " $(echo $RES | jq -c -r '.result') $NAME
 		DURATION=`ffprobe -i $P$NAME -show_entries format=duration -v quiet -of csv="p=0"`
 		DURATION25=`echo "$DURATION*0.25" | bc`
 		DURATION50=`echo "$DURATION*0.50" | bc`
 		DURATION75=`echo "$DURATION*0.75" | bc`
-		ffmpeg -y -i $P$NAME -f mjpeg -ss $DURATION25 -vframes 1 -s 640x480 -an $P$NAMEBASE-25.jpg	
-		ffmpeg -y -i $P$NAME -f mjpeg -ss $DURATION50 -vframes 1 -s 640x480 -an $P$NAMEBASE-50.jpg	
-		ffmpeg -y -i $P$NAME -f mjpeg -ss $DURATION75 -vframes 1 -s 640x480 -an $P$NAMEBASE-75.jpg	
+		ffmpeg -y -i $P$NAME -f mjpeg -ss $DURATION25 -vframes 1 -pix_fmt yuvj420p -an $P$NAMEBASE-25.jpg	
+		ffmpeg -y -i $P$NAME -f mjpeg -ss $DURATION50 -vframes 1 -pix_fmt yuvj420p -an $P$NAMEBASE-50.jpg	
+		ffmpeg -y -i $P$NAME -f mjpeg -ss $DURATION75 -vframes 1 -pix_fmt yuvj420p -an $P$NAMEBASE-75.jpg	
         fi
 fi
 done
