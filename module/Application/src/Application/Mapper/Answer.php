6f3074af6fcb4f576ce133a4ce89a9dddaca97d7
Christophe Robert <crobert@thestudnet.com>
2015-09-01 18:11:27 +0200

    #485 - eqcq.getList
    champ type correspond à un subcomponent. j'ai mis un num arbitraire les eq seraient entre 100 et 199 et les cq entre 200 et 299 (ca permet de mettre autant de sous composants qu'on veut et de les classer facilement)  var fakeEqcq = [ {"user": {"gender": "female"}, "score": 0.8, type: 101}, {"user": {"gender": "female"}, "score": 0.6, type: 101}, {"user": {"gender": "male"}, "score": 0.4, type: 101}, {"user": {"gender": "female"}, "score": 1, type: 102}, {"user": {"gender": "female"}, "score": 0.2, type: 102}, {"user": {"gender": "male"}, "score": 0.4, type: 102}, {"user": {"gender": "male"}, "score": 0.6, type: 102}, {"user": {"gender": "female"}, "score": 1, type: 103}, {"user": {"gender": "male"}, "score": 0.6, type: 103}, {"user": {"gender": "male"}, "score": 0.4, type: 103}, {"user": {"gender": "male"}, "score": 0.6, type: 103}, {"user": {"gender": "female"}, "score": 1, type: 104}, {"user": {"gender": "female"}, "score": 0.6, type: 104}, {"user": {"gender": "female"}, "score": 0.4, type: 104}, {"user": {"gender": "male"}, "score": 0.6, type: 104} ]; #done

--- module/Application/src/Application/Mapper/Answer.php
+++ module/Application/src/Application/Mapper/Answer.php
@@ -1,9 +1,32 @@
 <?php
-
 namespace Application\Mapper;
 
 use Dal\Mapper\AbstractMapper;
 
 class Answer extends AbstractMapper
 {
+    public function getList($item_prog, $peer)
+    {
+        $select = $this->tableGateway->getSql()->select();
+        $select->columns(array('id','peer_id','type','created_date'))
+            ->join('scale', 'scale.id=answer.scale_id', array('answer$scale' => 'value'))
+            ->join('question', 'question.id=answer.question_id', array('answer$component' => 'component_id'))
+            ->join('component', 'component.id=question.component_id', array('answer$dimension' => 'dimension_id'))
+            ->join('questionnaire_user', 'questionnaire_user.id=answer.questionnaire_user_id', array())
+            ->join('user', 'user.id=questionnaire_user.user_id', array('answer$gender' => 'gender'))
+            ->join('country', 'country.id=user.nationality', array('answer$nationality' => 'id','answer$nationality_name' => 'short_name'), $select::JOIN_LEFT)
+            ->join(array('origin' => 'country'), 'country.id=user.origin', array('answer$origin' => 'id','answer$origin_name' => 'short_name'), $select::JOIN_LEFT);
+        
+        if (null !== $peer) {
+            $select->where(array('answer.peer_id' => $peer));
+        }
+        
+        if (null !== $item_prog) {
+            $select->join('questionnaire', 'questionnaire.id=questionnaire_user.questionnaire_id', array())
+                ->join('item_prog', 'item_prog.item_id=questionnaire.item_id', array())
+                ->where(array('item_prog.id' => $item_prog));
+        }
+        
+        return $this->selectWith($select);
+    }
 }
--- module/Application/src/Application/Model/Answer.php
+++ module/Application/src/Application/Model/Answer.php
@@ -6,4 +6,108 @@ use Application\Model\Base\Answer as BaseAnswer;
 
 class Answer extends BaseAnswer
 {
+    protected $scale;
+    protected $component;
+    protected $dimension;
+    protected $gender;
+    protected $nationality;
+    protected $nationality_name;
+    protected $origin;
+    protected $origin_name;
+    
+    public function getOriginName() 
+    {
+        return $this->origin_name;
+    }
+
+    public function setOriginName($origin_name) 
+    {
+        $this->origin_name = $origin_name;
+        
+        return $this;
+    }
+    
+    public function getOrigin() 
+    {
+        return $this->origin;
+    }
+     
+    public function setOrigin($origin) 
+    {
+        $this->origin = $origin;
+        
+        return $this;
+    }
+
+    public function getNationalityName() 
+    {
+        return $this->nationality_name;
+    }
+     
+    public function setNationalityName($nationality_name) 
+    {
+        $this->nationality_name = $nationality_name;
+        
+        return $this;
+    }
+
+    public function getNationality() 
+    {
+        return $this->nationality;
+    }
+     
+    public function setNationality($nationality) 
+    {
+        $this->nationality = $nationality;
+        
+        return $this;
+    }
+    
+    public function getGender() 
+    {
+        return $this->gender;
+    }
+
+    public function setGender($gender) 
+    {
+        $this->gender = $gender;
+        
+        return $this;
+    }
+    
+    public function getDimension() 
+    {
+        return $this->dimension;
+    }
+     
+    public function setDimension($dimension) 
+    {
+        $this->dimension = $dimension;
+        
+        return $this;
+    }
+
+    public function getComponent() 
+    {
+        return $this->component;
+    }
+     
+    public function setComponent($component) 
+    {
+        $this->component = $component;
+        
+        return $this;
+    }
+    
+    public function getScale() 
+    {
+        return $this->scale;
+    }
+     
+    public function setScale($scale) 
+    {
+        $this->scale = $scale;
+        
+        return $this;
+    }
 }
