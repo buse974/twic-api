1a3cef12ada29e85ba26ef86235a21b87aaae968
Christophe Robert <crobert@thestudnet.com>
2015-09-02 14:55:21 +0200

    created date format

--- module/Application/src/Application/Mapper/Questionnaire.php
+++ module/Application/src/Application/Mapper/Questionnaire.php
@@ -1,9 +1,17 @@
 <?php
-
 namespace Application\Mapper;
 
 use Dal\Mapper\AbstractMapper;
+use Zend\Db\Sql\Expression;
 
 class Questionnaire extends AbstractMapper
 {
+    public function getByItem($item)
+    {
+        $select = $this->tableGateway->getSql()->select();
+        $select->columns(array('id','item_id','max_duration','max_time','questionnaire$created_date' => new Expression('DATE_FORMAT(questionnaire.created_date, "%Y-%m-%dT%TZ")')))
+            ->where(array('questionnaire.item_id' => $item));
+        
+        return $this->selectWith($select);
+    }
 }
