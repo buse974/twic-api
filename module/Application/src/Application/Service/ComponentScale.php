611f0766eed38f9c34b4c1ad4d389ebfc1036838
Arthur Flachs <aflachs@thestudnet.com>
2015-09-09 08:17:24 +0000

    component.getListWithScale

--- module/Application/src/Application/Service/ComponentScale.php
+++ module/Application/src/Application/Service/ComponentScale.php
@@ -6,4 +6,10 @@ use Dal\Service\AbstractService;
 
 class ComponentScale extends AbstractService
 {
+    public function getList($component_id)
+    {
+        $m_component_scale = $this->getModel()->setComponentId($component_id);
+
+        return $this->getMapper()->select($m_component_scale);
+    }
 }
