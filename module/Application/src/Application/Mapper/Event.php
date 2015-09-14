b232fc150247f8dc9d22a03f61407633facff36c
Paul BOUSSEKEY <pboussekey@thestudnet.com>
2015-09-14 11:38:08 +0200

    Event.getList

--- module/Application/src/Application/Mapper/Event.php
+++ module/Application/src/Application/Mapper/Event.php
@@ -3,15 +3,22 @@
 namespace Application\Mapper;
 
 use Dal\Mapper\AbstractMapper;
+use Zend\Db\Sql\Expression;
 
 class Event extends AbstractMapper
 {
-     public function getList($me, $ids = null)
+     public function getList($me, $events = null, $ids = null)
     {
-        $select = $this->tableGateway->getSql()->select();
-        $select->join('event_user', 'event.id=event_user.event_id', array())
-            ->where(array('event_user.user_id' => $me));
+        $select = $this->tableGateway->getSql()->select()
+            ->columns(array("id", "source", "object", "event", 'event$date' => new Expression("DATE_FORMAT(date, '%Y-%m-%dT%TZ') ")))
+            ->join('event_user', 'event.id=event_user.event_id', array())
+            ->where(array('event_user.user_id' => $me))
+            ->order(array('event.id' => 'DESC'));
         
+        if(null !== $events){
+            $select->where(array('event.event' => $events));
+        }
+        //exit($this->printSql($select));
             
         return $this->selectWith($select);
     }
