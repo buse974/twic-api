87685c8366266c926472fcddf7a85d1741465590
Paul BOUSSEKEY <pboussekey@thestudnet.com>
2015-09-14 15:24:45 +0200

    Show global event

--- module/Application/src/Application/Mapper/Event.php
+++ module/Application/src/Application/Mapper/Event.php
@@ -4,17 +4,17 @@ namespace Application\Mapper;
 
 use Dal\Mapper\AbstractMapper;
 use Zend\Db\Sql\Expression;
-use Zend\Db\Sql\Predicate;
+use Zend\Db\Sql\Predicate\Predicate;
 
 class Event extends AbstractMapper
 {
      public function getList($me, $events = null, $ids = null)
     {
-        $select = $this->tableGateway->getSql()->select()
-            ->columns(array("id", "source", "object", "event", 'event$date' => new Expression("DATE_FORMAT(date, '%Y-%m-%dT%TZ') ")))
+        $select = $this->tableGateway->getSql()->select();
+        $select->columns(array("id", "source", "object", "event", 'event$date' => new Expression("DATE_FORMAT(date, '%Y-%m-%dT%TZ') ")))
             ->join('event_user', 'event.id=event_user.event_id', array(),  $select::JOIN_LEFT)
-            ->where(array('( event_user.user_id' => $me))
-            ->where(array(' event.target = "global")'), Predicate::OP_OR)
+            ->where(array(' (event_user.user_id = ?' => $me))
+            ->where(array(' event.target =  ?)' => "global"), Predicate::OP_OR)
             ->order(array('event.id' => 'DESC'));
         
         if(null !== $events){
