2994bc74c6dc1782d44c311b8cc227d9f370ea13
Arthur Flachs <aflachs@thestudnet.com>
2015-09-14 21:18:18 +0200

    Fetch language and level

--- module/Application/src/Application/Model/UserLanguage.php
+++ module/Application/src/Application/Model/UserLanguage.php
@@ -6,4 +6,26 @@ use Application\Model\Base\UserLanguage as BaseUserLanguage;
 
 class UserLanguage extends BaseUserLanguage
 {
+    protected $language;
+    protected $level;
+
+    public function setLanguage($language)
+    {
+        $this->language = $language;
+    }
+
+    public function getLanguage()
+    {
+        return $this->language;
+    }
+
+    public function setLevel($level)
+    {
+        $this->level = $level;
+    }
+
+    public function getLevel()
+    {
+        return $this->level;
+    }
 }
--- module/Application/src/Application/Service/UserLanguage.php
+++ module/Application/src/Application/Service/UserLanguage.php
@@ -37,6 +37,35 @@ class UserLanguage extends AbstractService
 
     /**
      * @invokable
+     */
+    public function get($user)
+    {
+        $m_user_langage = $this->getModel();
+        $m_user_langage->setUserId($user);
+
+        $languages = $this->getMapper()->select($m_user_langage);
+        foreach ($languages as $language) {
+            $m_language = $this->getServiceLanguage()->getModel();
+            $m_language->setId($language->getLanguageId());
+            $m_level = $this->getServiceLanguageLevel()->getModel();
+            $m_level->setId($language->getLanguageLevelId());
+            $language->setLanguage($this->getServiceLanguage()
+                ->getMapper()
+                ->select($m_language)
+                ->current()
+            );
+            $language->setLevel($this->getServiceLanguageLevel()
+                ->getMapper()
+                ->select($m_level)
+                ->current()
+            );
+        }
+
+        return $languages;
+    }
+
+    /**
+     * @invokable
      *
      * @param int $id
      *
@@ -45,6 +74,16 @@ class UserLanguage extends AbstractService
     public function delete($id)
     {
         return $this->getMapper()->delete($this->getModel()->setId($id)->setUserId($this->getServiceAuth()->getIdentity()->getId()));
+    }
+
+    public function getServiceLanguage()
+    {
+        return $this->getServiceLocator()->get('app_service_language');
+    }
+
+    public function getServiceLanguageLevel()
+    {
+        return $this->getServiceLocator()->get('app_service_language_level');
     }
 
     /**
