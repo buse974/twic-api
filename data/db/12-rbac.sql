dc2d133c264ec124f31a0a7e2bb21903e4187947
Arthur Flachs <aflachs@thestudnet.com>
2015-09-15 13:49:34 +0200

    Get Languages

--- data/db/12-rbac.sql
+++ data/db/12-rbac.sql
@@ -722,3 +722,7 @@ INSERT INTO `role_permission` (`role_id`, `permission_id`) VALUES (4, @rbac_perm
 INSERT INTO `permission` (`libelle`) VALUES ('userlanguage.get');
 SELECT LAST_INSERT_ID() INTO @rbac_permission_id;
 INSERT INTO `role_permission` (`role_id`, `permission_id`) VALUES (4, @rbac_permission_id);
+
+INSERT INTO `permission` (`libelle`) VALUES ('language.getList');
+SELECT LAST_INSERT_ID() INTO @rbac_permission_id;
+INSERT INTO `role_permission` (`role_id`, `permission_id`) VALUES (4, @rbac_permission_id);
