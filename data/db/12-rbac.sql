70143d159d7326cb87d45e5b68612e6b0145317d
Christophe Robert <christophe.robert@babblecar.com>
2015-09-15 14:37:09 +0200

    rbac

--- data/db/12-rbac.sql
+++ data/db/12-rbac.sql
@@ -695,6 +695,9 @@ SELECT LAST_INSERT_ID() INTO @rbac_permission_id;
 INSERT INTO `role_permission` (`role_id`, `permission_id`) VALUES (4, @rbac_permission_id);
 
 INSERT INTO `permission` (`libelle`) VALUES ('itemproguser.getStartedConference');
+SELECT LAST_INSERT_ID() INTO @rbac_permission_id;
+INSERT INTO `role_permission` (`role_id`, `permission_id`) VALUES (4, @rbac_permission_id);
+
 INSERT INTO `permission` (`libelle`) VALUES ('event.getList');
 SELECT LAST_INSERT_ID() INTO @rbac_permission_id;
 INSERT INTO `role_permission` (`role_id`, `permission_id`) VALUES (4, @rbac_permission_id);
