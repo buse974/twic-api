2b63f0d0fa3f38cec49dce9fc1789eabbddb54af
Paul BOUSSEKEY <pboussekey@thestudnet.com>
2015-09-15 10:13:24 +0200

    Thread.message, fix structure

--- module/Application/src/Application/Mapper/Event.php
+++ module/Application/src/Application/Mapper/Event.php
@@ -10,18 +10,11 @@ class Event extends AbstractMapper
 {
      public function getList($me, $events = null, $id = null)
     {
-<<<<<<< HEAD
         $select = $this->tableGateway->getSql()->select();
         $select->columns(array("id", "source", "object", "event", 'event$date' => new Expression("DATE_FORMAT(date, '%Y-%m-%dT%TZ') ")))
             ->join('event_user', 'event.id=event_user.event_id', array(),  $select::JOIN_LEFT)
             ->where(array(' (event_user.user_id = ?' => $me))
             ->where(array(' event.target =  ?)' => "global"), Predicate::OP_OR)
-=======
-        $select = $this->tableGateway->getSql()->select()
-            ->columns(array("id", "source", "object", "event", 'event$date' => new Expression("DATE_FORMAT(date, '%Y-%m-%dT%TZ') ")))
-            ->join('event_user', 'event.id=event_user.event_id', array())
-            
->>>>>>> 59a39a87256f1e1b7c624a938f61210f23d86b6f
             ->order(array('event.id' => 'DESC'));
         
         if(null !== $events){
