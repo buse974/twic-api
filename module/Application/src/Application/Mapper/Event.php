fecb3e0e22a84296d4ad57e951f1cf8575ee1ece
Christophe Robert <crobert@thestudnet.com>
2015-09-15 10:06:05 +0200

    event

--- module/Application/src/Application/Mapper/Event.php
+++ module/Application/src/Application/Mapper/Event.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Application\Mapper;
 
 use Dal\Mapper\AbstractMapper;
@@ -7,20 +6,21 @@ use Zend\Db\Sql\Expression;
 
 class Event extends AbstractMapper
 {
-     public function getList($me, $events = null, $id = null)
+
+    public function getList($me, $events = null, $id = null)
     {
-        $select = $this->tableGateway->getSql()->select()
-            ->columns(array("id", "source", "object", "event", 'event$date' => new Expression("DATE_FORMAT(date, '%Y-%m-%dT%TZ') ")))
+        $select = $this->tableGateway->getSql()->select();
+        $select->columns(array("id","source","object","event",'event$date' => new Expression("DATE_FORMAT(date, '%Y-%m-%dT%TZ') ")))
             ->join('event_user', 'event.id=event_user.event_id', array())
-            
+            ->join('like', 'event.id=like.event_id', array('event$nb_like' => new Expression('COUNT(like.event_id)'),'event$is_like' => new Expression('MAX(IF(like.user_id = ' . $me . ', 1, 0))')), $select::JOIN_LEFT)
+            ->group('event.id')
             ->order(array('event.id' => 'DESC'));
         
-        if(null !== $events){
-            $select->where(array('event.event' => $events))
-            ->where(array('event_user.user_id' => $me));
+        if (null !== $events) {
+            $select->where(array('event.event' => $events))->where(array('event_user.user_id' => $me));
         }
         
-        if(null !== $id){
+        if (null !== $id) {
             $select->where(array('event.id' => $id));
         }
         
