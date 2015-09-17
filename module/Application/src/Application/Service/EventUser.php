f0f48be573fb3dbda0d3ffe8a5c7983cf4504287
Paul BOUSSEKEY <pboussekey@thestudnet.com>
2015-09-17 17:01:19 +0200

    eventuser => read

--- module/Application/src/Application/Service/EventUser.php
+++ module/Application/src/Application/Service/EventUser.php
@@ -20,4 +20,35 @@ class EventUser extends AbstractService
     
         return true;
     }
+    
+     /**
+     * @invokable
+     */
+    public function read($ids = null)
+    {
+        $date = (new \DateTime('now', new \DateTimeZone('UTC')))->format('Y-m-d H:i:s');
+        $me = $this->getServiceUser()->getIdentity()['id'];  
+        $m_event = $this->getModel()->setUserId($me)->setReadDate($date);
+        if(null !== $ids){
+            $nb = 0;
+            foreach($ids as $id){
+               $nb += $this->getMapper()->update($m_event->setEventId($id));
+            }
+            return $nb;
+        }
+        else{
+            
+            return $this->getMapper()->update($m_event, array('user_id' => $me));
+        }
+        
+    }
+    
+      /**
+     *
+     * @return \Application\Service\User
+     */
+    public function getServiceUser()
+    {
+        return $this->getServiceLocator()->get('app_service_user');
+    }
 }
