a794b1f54b07379d6b48b1bfa2c31f472096a1ff
Christophe Robert <christophe.robert@babblecar.com>
2015-09-22 11:29:46 +0200

    event user read test ok

--- module/Application/src/Application/Service/EventUser.php
+++ module/Application/src/Application/Service/EventUser.php
@@ -1,49 +1,43 @@
 <?php
-
 namespace Application\Service;
 
 use Dal\Service\AbstractService;
 
 class EventUser extends AbstractService
 {
+
     public function add($user, $notification)
     {
-        if(!is_array($user)) {
+        if (! is_array($user)) {
             $user = [$user];
         }
         $m_event_user = $this->getModel()->setEventId($notification);
-    
+        
         foreach ($user as $u) {
             $m_event_user->setUserId($u);
             $this->getMapper()->insert($m_event_user);
         }
-    
+        
         return true;
     }
-    
-     /**
+
+    /**
      * @invokable
      */
     public function read($ids = null)
     {
-        $date = (new \DateTime('now', new \DateTimeZone('UTC')))->format('Y-m-d H:i:s');
-        $me = $this->getServiceUser()->getIdentity()['id'];  
-        $m_event = $this->getModel()->setUserId($me)->setReadDate($date);
-        if(null !== $ids){
-            $nb = 0;
-            foreach($ids as $id){
-               $nb += $this->getMapper()->update($m_event->setEventId($id));
-            }
-            return $nb;
-        }
-        else{
-            
-            return $this->getMapper()->update($m_event, array('user_id' => $me));
-        }
+        $me = $this->getServiceUser()->getIdentity()['id'];
         
+        $m_event = $this->getModel()
+            ->setUserId($me)
+            ->setReadDate((new \DateTime('now', new \DateTimeZone('UTC')))->format('Y-m-d H:i:s'));
+        
+        return (null !== $ids) ?
+            $this->getMapper()->update($m_event->setEventId($ids)) : 
+            $nb = $this->getMapper()->update($m_event, array('user_id' => $me));
     }
-    
-      /**
+
+    /**
      *
      * @return \Application\Service\User
      */
