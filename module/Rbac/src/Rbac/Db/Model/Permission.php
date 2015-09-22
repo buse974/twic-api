98fbd7259146d344806f9a45c049ad4d45e35da7
Christophe Robert <christophe.robert@babblecar.com>
2015-09-22 17:45:21 +0200

    permission

--- module/Rbac/src/Rbac/Db/Model/Permission.php
+++ module/Rbac/src/Rbac/Db/Model/Permission.php
@@ -7,19 +7,34 @@ use Rbac\Db\Model\Base\Permission as BasePermission;
 class Permission extends BasePermission
 {
     protected $role;
+    protected $role_permission;
 
     public function exchangeArray(array &$data)
     {
         if ($this->isRepeatRelational()) {
             return;
         }
-
         parent::exchangeArray($data);
         
         $this->role = new Role($this);
+        $this->role_permission = new RolePermission($this);
+        
+        $this->role_permission->exchangeArray($data);
         $this->role->exchangeArray($data);
     }
 
+    public function getRolePermission()
+    {
+        return $this->role_permission;
+    }
+    
+    public function setRolePermission($role_permission)
+    {
+        $this->role_permission = $role_permission;
+    
+        return $this;
+    }
+    
     public function setRole($role)
     {
         $this->role = $role;
