e068c27cec80e50b62fdea05c614f4193ea9f786
Christophe Robert <crobert@thestudnet.com>
2015-09-23 17:08:22 +0200

    activity.getList

--- data/db/12-rbac.sql
+++ data/db/12-rbac.sql
@@ -779,6 +779,10 @@ INSERT INTO `permission` (`libelle`) VALUES ('eventuser.read');
 SELECT LAST_INSERT_ID() INTO @rbac_permission_id;
 INSERT INTO `role_permission` (`role_id`, `permission_id`) VALUES (4, @rbac_permission_id);
 
-INSERT INTO `permission` (`libelle`) VALUES ('activity.read');
+INSERT INTO `permission` (`libelle`) VALUES ('activity.add');
+SELECT LAST_INSERT_ID() INTO @rbac_permission_id;
+INSERT INTO `role_permission` (`role_id`, `permission_id`) VALUES (4, @rbac_permission_id);
+
+INSERT INTO `permission` (`libelle`) VALUES ('activity.getList');
 SELECT LAST_INSERT_ID() INTO @rbac_permission_id;
 INSERT INTO `role_permission` (`role_id`, `permission_id`) VALUES (4, @rbac_permission_id);
--- module/Application/src/Application/Service/Activity.php
+++ module/Application/src/Application/Service/Activity.php
@@ -65,4 +65,60 @@ class Activity extends AbstractService
         
         return $this->getMapper()->getLastInsertValue();
     }
+    
+    /**
+     * @invokable
+     * 
+     * @param array $source
+     * @param string $date
+     * @param string $event
+     * @param array $object
+     * @param array $target
+     * @throws \Exception
+     * 
+     * @return integer
+     * 
+     */
+    public function getList($source = null, $date = null, $event = null, $object = null, $target = null)
+    {
+        $m_activity = $this->getModel();
+        $m_activity->setEvent($event);
+        $m_activity->setDate($date);
+        
+        if(null !== $source) {
+            if(isset($source['id'])) {
+                $m_activity->setSourceId($source['id']);
+            }
+            if(isset($source['name'])) {
+                $m_activity->setSourceName($source['name']);
+            }
+            if(isset($source['data'])) {
+                $m_activity->setSourceData($source['data']);
+            }
+        }
+        if(null !== $object) {
+            if(isset($object['id'])) {
+                $m_activity->setObjectId($object['id']);
+            }
+            if(isset($object['name'])) {
+                $m_activity->setObjectName($object['name']);
+            }
+            if(isset($object['data'])) {
+                $m_activity->setObjectData($object['data']);
+            }
+        }
+        if(null !== $target) {
+            if(isset($target['id'])) {
+                $m_activity->setTargetId($target['id']);
+            }
+            if(isset($target['name'])) {
+                $m_activity->setTargetName($target['name']);
+            }
+            if(isset($target['data'])) {
+                $m_activity->setTargetData($target['data']);
+            }
+        }
+        
+        return $this->getMapper()->select($m_activity);
+    }
 }
