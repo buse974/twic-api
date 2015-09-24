1b3a9a9f5625dba190821114353e0dd1f2d5b9d5
Christophe Robert <crobert@thestudnet.com>
2015-09-24 10:16:10 +0200

    grading

--- module/Application/src/Application/Mapper/GradingPolicy.php
+++ module/Application/src/Application/Mapper/GradingPolicy.php
@@ -14,9 +14,8 @@ class GradingPolicy extends AbstractMapper
 
         $select->columns(array('id', 'name', 'grade', 'grading_policy$nbr_comment' => new Expression('CAST(SUM(IF(grading_policy_grade_comment.id IS NOT NULL, 1, 0)) AS DECIMAL )')))
                 
-                ->join('user', 'user.id=user.id', array(), $select::JOIN_INNER)
-                ->join('grading_policy_grade', 'grading_policy_grade.grading_policy_id=grading_policy.id 
-                    AND grading_policy_grade.user_id=user.id', array('grade'), $select::JOIN_LEFT)
+                ->join('user', 'user.id=user.id', array(), $select::JOIN_CROSS)
+                ->join('grading_policy_grade', 'grading_policy_grade.grading_policy_id=grading_policy.id AND grading_policy_grade.user_id=user.id', array('grade'), $select::JOIN_LEFT)
                     
                 
                 ->join('grading_policy_grade_comment', 'grading_policy_grade_comment.grading_policy_grade_id=grading_policy_grade.id', array(), $select::JOIN_LEFT)
