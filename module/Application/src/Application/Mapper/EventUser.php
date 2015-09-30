0f66bb98155efc7aa5f1cf7de32d552884ae8798
Christophe Robert <christophe.robert@babblecar.com>
2015-09-30 11:06:50 +0200

    event

--- module/Application/src/Application/Mapper/EventUser.php
+++ module/Application/src/Application/Mapper/EventUser.php
@@ -6,4 +6,14 @@ use Dal\Mapper\AbstractMapper;
 
 class EventUser extends AbstractMapper
 {
+    public function inserUpdate($date, $me)
+    {
+        $sql = "INSERT INTO event_user (event_id, user_id, read_date)
+                SELECT event.id as event_id, :user as user_id, :read_date as read_date FROM event_user
+                RIGHT JOIN event ON event.id=event_user.event_id
+                WHERE event.target <> 'user' AND
+                event_user.read_date IS NULL";
+        
+        return $this->requestPdo($sql, ['user' => $me, 'read_date' => $date]);
+    }
 }
