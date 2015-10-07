378247b38cdd6b937ed6eac7e12def7a16400232
Christophe Robert <crobert@thestudnet.com>
2015-10-07 17:00:30 +0200

    event ok

--- module/Application/src/Application/Mapper/EventUser.php
+++ module/Application/src/Application/Mapper/EventUser.php
@@ -1,19 +1,45 @@
 <?php
-
 namespace Application\Mapper;
 
 use Dal\Mapper\AbstractMapper;
+use Zend\Db\Sql\Predicate\Expression;
+use Dal\Db\Sql\Select;
 
 class EventUser extends AbstractMapper
 {
-    public function inserUpdate($date, $me)
+    public function insertUpdate($date, $me, $event = null)
     {
-        $sql = "INSERT INTO event_user (event_id, user_id, read_date)
-                SELECT event.id as event_id, :user as user_id, :read_date as read_date FROM event_user
-                RIGHT JOIN event ON event.id=event_user.event_id
-                WHERE event.target <> 'user' AND
-                event_user.read_date IS NULL";
+        $select = $this->tableGateway->getSql()->select();
+        
+        $select->columns(array('user_id' => new Expression("'$me'"),'read_date' => new Expression("'$date'")))
+            ->join('event', 'event.id=event_user.event_id', array('event_id' => 'id'), $select::JOIN_RIGHT)
+            ->where(array("event.target <> 'user'","event_user.read_date IS NULL"));
+        
+        if (null !== $event) {
+            $select->where(array("event.event" => $event));
+        }
+        
+        $insert = $this->tableGateway->getSql()->insert();
+        
+        $insert->columns(array('user_id','read_date','event_id'))->select($select);
+        
+        return $this->insertWith($insert);
+    }
 
-        return $this->requestPdo($sql, ['user' => $me, 'read_date' => $date]);
+    public function updateReadMe($date, $me, $event = null)
+    {
+        $select = new Select('event');
+        $select->columns(array('id'));
+        if (null !== $event) {
+            $select->where(array("event.event" => $event));
+        }
+        
+        $update = $this->tableGateway->getSql()->update();
+        $update->set(array('read_date' => $date))
+            ->where(array('event_id' => $select))
+            ->where(array('user_id' => $me))
+            ->where(array('read_date IS NULL'));
+        
+        return $this->updateWith($update);
     }
 }
