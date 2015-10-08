cbc370e35c812dba211e2cb7873906b1f4d455dc
Christophe Robert <christophe.robert@babblecar.com>
2015-10-08 16:59:19 +0200

    dmension compoent

--- module/Application/src/Application/Model/ComponentScale.php
+++ module/Application/src/Application/Model/ComponentScale.php
@@ -6,4 +6,24 @@ use Application\Model\Base\ComponentScale as BaseComponentScale;
 
 class ComponentScale extends BaseComponentScale
 {
+    protected $component;
+    
+    public function exchangeArray(array &$data)
+    {
+        parent::exchangeArray($data);
+    
+        $this->component = $this->requireModel('app_model_component', $data);
+    }
+    
+    public function getComponent()
+    {
+        return $this->component;
+    }
+    
+    public function setComponent($component)
+    {
+        $this->component = $component;
+    
+        return $this;
+    }
 }
