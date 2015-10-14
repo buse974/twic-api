dfdf6a659160786807cb26ba41bcacb262b8bbd2
Christophe Robert <christophe.robert@babblecar.com>
2015-10-14 10:19:50 +0200

    guidelines ok

--- data/guidelines.php
+++ data/guidelines.php
@@ -4,7 +4,7 @@
 $file = json_decode(file_get_contents("guidelines.json"), true);
 foreach($file as $k => $v) {
     foreach($v as $v1) {
-	$value = json_encode($v);
+	$value = json_encode($v1);
 	$value = str_replace("'","''",$value);
 	$value = str_replace("\\","\\\\",$value);
         echo "INSERT INTO `guidelines`(`state`,`data`)VALUES('$k','$value');\n";
