594b539d389bff97a83fcf5ff606c7af00be4f7a
Christophe Robert <christophe.robert@babblecar.com>
2015-10-19 11:55:08 +0200

    sql eqcq

--- data/db/12-rbac.sql
+++ data/db/12-rbac.sql
@@ -373,7 +373,7 @@ INSERT INTO `role_permission` (`role_id`, `permission_id`) VALUES (1, @rbac_perm
 
 INSERT INTO `permission` (`libelle`) VALUES ('school.update');
 SELECT LAST_INSERT_ID() INTO @rbac_permission_id;
-INSERT INTO `role_permission` (`role_id`, `permission_id`) VALUES (1, @rbac_permission_id);
+INSERT INTO `role_permission` (`role_id`, `permission_id`) VALUES (3, @rbac_permission_id);
 
 INSERT INTO `permission` (`libelle`) VALUES ('school.delete');
 SELECT LAST_INSERT_ID() INTO @rbac_permission_id;
