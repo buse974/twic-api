0417d1e1029920c8d00e60b9cb3ffa126e2166f4
Christophe Robert <crobert@thestudnet.com>
2015-10-26 17:52:58 +0100

    right

--- data/db/12-rbac.sql
+++ data/db/12-rbac.sql
@@ -634,6 +634,10 @@ INSERT INTO `permission` (`libelle`) VALUES ('thread.getNbrMessage');
 SELECT LAST_INSERT_ID() INTO @rbac_permission_id;
 INSERT INTO `role_permission` (`role_id`, `permission_id`) VALUES (4, @rbac_permission_id);
 
+INSERT INTO `permission` (`libelle`) VALUES ('message.getNbrMessage');
+SELECT LAST_INSERT_ID() INTO @rbac_permission_id;
+INSERT INTO `role_permission` (`role_id`, `permission_id`) VALUES (4, @rbac_permission_id);
+
 INSERT INTO `permission` (`libelle`) VALUES ('thread.update');
 SELECT LAST_INSERT_ID() INTO @rbac_permission_id;
 INSERT INTO `role_permission` (`role_id`, `permission_id`) VALUES (4, @rbac_permission_id);
