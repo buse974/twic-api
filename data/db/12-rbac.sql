044e7cb8b6b95d8084268bca8c2935cd9a1e4513
Paul BOUSSEKEY <pboussekey@thestudnet.com>
2015-10-28 14:58:56 +0100

    Event comment

--- data/db/12-rbac.sql
+++ data/db/12-rbac.sql
@@ -876,3 +876,15 @@ INSERT INTO `role_permission` (`role_id`, `permission_id`) VALUES (5, @rbac_perm
 INSERT INTO `permission` (`libelle`) VALUES ('materialdocument.getListByItem');
 SELECT LAST_INSERT_ID() INTO @rbac_permission_id;
 INSERT INTO `role_permission` (`role_id`, `permission_id`) VALUES (5, @rbac_permission_id);
+
+INSERT INTO `permission` (`libelle`) VALUES ('eventcomment.add');
+SELECT LAST_INSERT_ID() INTO @rbac_permission_id;
+INSERT INTO `role_permission` (`role_id`, `permission_id`) VALUES (4, @rbac_permission_id);
+
+INSERT INTO `permission` (`libelle`) VALUES ('eventcomment.update');
+SELECT LAST_INSERT_ID() INTO @rbac_permission_id;
+INSERT INTO `role_permission` (`role_id`, `permission_id`) VALUES (4, @rbac_permission_id);
+
+INSERT INTO `permission` (`libelle`) VALUES ('eventcomment.delete');
+SELECT LAST_INSERT_ID() INTO @rbac_permission_id;
+INSERT INTO `role_permission` (`role_id`, `permission_id`) VALUES (4, @rbac_permission_id);
--- module/Application/src/Application/Model/EventComment.php
+++ module/Application/src/Application/Model/EventComment.php
@@ -6,4 +6,24 @@ use Application\Model\Base\EventComment as BaseEventComment;
 
 class EventComment extends BaseEventComment
 {
+    protected $user;
+
+    public function exchangeArray(array &$data)
+    {
+        parent::exchangeArray($data);
+
+        $this->user = $this->requireModel('app_model_user', $data);
+    }
+
+    public function setUser($user)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getUser()
+    {
+        return $this->user;
+    }
 }
