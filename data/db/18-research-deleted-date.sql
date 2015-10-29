b5653988164e3382883561841c0f250589f8e8c6
Christophe Robert <christophe.robert@babblecar.com>
2015-10-29 10:43:05 +0100

    deleted_date resaerch

---
+++ data/db/18-research-deleted-date.sql
@@ -0,0 +1,47 @@
+CREATE 
+     OR REPLACE ALGORITHM = UNDEFINED 
+    DEFINER = `root`@`localhost` 
+    SQL SECURITY DEFINER
+VIEW `research` AS
+    SELECT 
+        `u`.`id` AS `id`,
+        CONCAT(`u`.`firstname`, ' ', `u`.`lastname`) AS `firstname`,
+        CONCAT(`u`.`lastname`, ' ', `u`.`firstname`) AS `lastname`,
+        `u`.`avatar` AS `avatar`,
+        'user' AS `category`,
+        `role`.`name` AS `role`,
+        IF((`role`.`id` = 4), 10, 20) AS `facette`
+    FROM
+        ((`user` `u`
+        JOIN `user_role` `us`)
+        JOIN `role`)
+    WHERE
+        ((`us`.`user_id` = `u`.`id`)
+            AND (`us`.`role_id` = `role`.`id`)
+            AND (`us`.`role_id` IN (4 , 5)) AND ISNULL(`u`.`deleted_date`)) 
+    UNION SELECT 
+        `c`.`id` AS `id`,
+        `c`.`title` AS `title`,
+        `p`.`name` AS `name`,
+        `c`.`picture` AS `picture`,
+        'course' AS `category`,
+        '' AS `role`,
+        '30' AS `facette`
+    FROM
+        (`course` `c`
+        JOIN `program` `p`)
+    WHERE
+        ((`c`.`program_id` = `p`.`id`)
+            AND ISNULL(`p`.`deleted_date`) AND ISNULL(`c`.`deleted_date`)) 
+    UNION SELECT 
+        `s`.`id` AS `id`,
+        `s`.`name` AS `name`,
+        `s`.`short_name` AS `short_name`,
+        `s`.`logo` AS `logo`,
+        'school' AS `category`,
+        '' AS `role`,
+        '40' AS `facette`
+    FROM
+        `school` `s` 
+	WHERE ISNULL(`s`.`deleted_date`);
+
