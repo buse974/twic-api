404d74f83e0d20ad630cd251bcee1645efaac0d0
Christophe Robert <christophe.robert@thestudnet.com>
2015-11-11 16:21:15 +0100

    event

--- module/Application/src/Application/Mapper/EventUser.php
+++ module/Application/src/Application/Mapper/EventUser.php
@@ -10,21 +10,22 @@ class EventUser extends AbstractMapper
 {
     public function insertUpdate($date, $me, $event = null)
     {
-        $select = $this->tableGateway->getSql()->select();
-
-        $select->columns(array('user_id' => new Expression("'$me'"), 'read_date' => new Expression("'$date'")))
-            ->join('event', 'event.id=event_user.event_id', array('event_id' => 'id'), $select::JOIN_RIGHT)
-            ->where(array("event.target <> 'user'", ' ( event_user.read_date IS NULL AND event_user.view_date IS NULL ) '));
-
+        $select = new Select('event');
+        
+        $select->columns(array('user_id' => new Expression("'$me'"), 'read_date' => new Expression("'$date'"), 'event_id' => 'id'))
+            ->join('user', 'user.id=user.id', [])
+            ->join('event_user', 'event.id = event_user.event_id AND user.id=event_user.user_id', [], $select::JOIN_LEFT)
+            ->where(array('user.id' => $me))
+            ->where(array("event.target <> 'user' AND event_user.user_id IS NULL "));
+        
         if (null !== $event) {
             $select->where(array('event.event' => $event));
         }
 
         $insert = $this->tableGateway->getSql()->insert();
-
         $insert->columns(array('user_id', 'read_date', 'event_id'))->select($select);
 
-        return $this->insertWith($insert);
+        return $this->insertWith($insert);    
     }
 
     public function updateReadMe($date, $me, $event = null)
