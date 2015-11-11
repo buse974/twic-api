e42c3accc93cb7fcd5f9e6ba3dc04af640ee9b81
Christophe Robert <christophe.robert@thestudnet.com>
2015-11-11 16:30:15 +0100

    event_user

--- module/Application/src/Application/Mapper/EventUser.php
+++ module/Application/src/Application/Mapper/EventUser.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Application\Mapper;
 
 use Dal\Mapper\AbstractMapper;
@@ -8,11 +7,12 @@ use Dal\Db\Sql\Select;
 
 class EventUser extends AbstractMapper
 {
+
     public function insertUpdate($date, $me, $event = null)
     {
         $select = new Select('event');
         
-        $select->columns(array('user_id' => new Expression("'$me'"), 'read_date' => new Expression("'$date'"), 'event_id' => 'id'))
+        $select->columns(array('user_id' => new Expression("'$me'"),'read_date' => new Expression("'$date'"),'event_id' => 'id'))
             ->join('user', 'user.id=user.id', [])
             ->join('event_user', 'event.id = event_user.event_id AND user.id=event_user.user_id', [], $select::JOIN_LEFT)
             ->where(array('user.id' => $me))
@@ -21,27 +21,29 @@ class EventUser extends AbstractMapper
         if (null !== $event) {
             $select->where(array('event.event' => $event));
         }
-
+        
         $insert = $this->tableGateway->getSql()->insert();
-        $insert->columns(array('user_id', 'read_date', 'event_id'))->select($select);
-
-        return $this->insertWith($insert);    
+        $insert->columns(array('user_id','read_date','event_id'))->select($select);
+        
+        return $this->insertWith($insert);
     }
 
     public function updateReadMe($date, $me, $event = null)
     {
-        $select = new Select('event');
-        $select->columns(array('id'));
-        if (null !== $event) {
-            $select->where(array('event.event' => $event));
-        }
-
         $update = $this->tableGateway->getSql()->update();
         $update->set(array('read_date' => $date))
-            ->where(array('event_id IN ?' => $select))
             ->where(array('user_id' => $me))
             ->where(array('read_date IS NULL'));
-
+        
+        if (null !== $event) {
+            $select = new Select('event');
+            $select->columns(array('id'));
+            if (null !== $event) {
+                $select->where(array('event.event' => $event));
+            }
+            $update->where(array('event_id IN ?' => $select));
+        }
+        
         return $this->updateWith($update);
     }
 }
