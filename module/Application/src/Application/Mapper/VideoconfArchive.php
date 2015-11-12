c3d00ebfec0232ccd0e7b4ca8473c3477aed1bb2
Christophe Robert <christophe.robert@thestudnet.com>
2015-11-12 14:25:27 +0100

    #730 - item_prog.getVideos
    retourne les vidéoconf_archive (videoconf.start_date, videoconf.archive_link, videoconf.duration, videoconf.title, videoconf.description, videoconf_archive.archive_link, videoconf.archive_duration) pour un item_prog donné, où archive_status == 'available' #done

--- module/Application/src/Application/Mapper/VideoconfArchive.php
+++ module/Application/src/Application/Mapper/VideoconfArchive.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Application\Mapper;
 
 use Dal\Mapper\AbstractMapper;
@@ -7,40 +6,43 @@ use Application\Model\Videoconf as CVF;
 
 class VideoconfArchive extends AbstractMapper
 {
+
     public function getListVideoUpload()
     {
         $select = $this->tableGateway->getSql()->select();
-
-        $select->columns(array('id', 'archive_token', 'archive_link', 'archive_status'))->where(array('archive_status' => CVF::ARV_STARTED));
-
+        
+        $select->columns(array('id','archive_token','archive_link','archive_status'))->where(array('archive_status' => CVF::ARV_STARTED));
+        
         return $this->selectWith($select);
     }
 
     public function getListRecordByItemProg($item_prog)
     {
         $select = $this->tableGateway->getSql()->select();
-
-        $select->columns(array('id', 'archive_link', 'archive_token', 'archive_duration'))
-        ->join('videoconf', 'videoconf.id=videoconf_archive.videoconf_id', array(), $select::JOIN_INNER)
-        ->where(array('videoconf.item_prog_id' => $item_prog));
-
+        
+        $select->columns(array('id','archive_link','archive_token','archive_duration'))
+            ->join('videoconf', 'videoconf.id=videoconf_archive.videoconf_id', array(), $select::JOIN_INNER)
+            ->where(array('videoconf.item_prog_id' => $item_prog))
+            ->where(array('archive_status' => CVF::ARV_AVAILABLE));
+        
         return $this->selectWith($select);
     }
 
     /**
-     * @param int $videoconf
-     * 
+     *
+     * @param int $videoconf            
+     *
      * @return \Application\Model\VideoconfArchive
      */
     public function getLastArchiveId($videoconf)
     {
         $select = $this->tableGateway->getSql()->select();
-
-        $select->columns(array('id', 'archive_link', 'archive_token', 'archive_duration'))
+        
+        $select->columns(array('id','archive_link','archive_token','archive_duration'))
             ->where(array('videoconf_archive.videoconf_id' => $videoconf))
             ->order(array('videoconf_archive.id' => 'DESC'))
             ->limit(1);
-
+        
         return $this->selectWith($select)->current();
     }
 }
