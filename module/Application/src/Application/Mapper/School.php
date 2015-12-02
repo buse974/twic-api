93e429c7bb107ca4740c4db617291c1594044f4f
Christophe Robert <christophe.robert@thestudnet.com>
2015-12-02 17:55:31 +0100

    school get

--- module/Application/src/Application/Mapper/School.php
+++ module/Application/src/Application/Mapper/School.php
@@ -19,10 +19,10 @@ class School extends AbstractMapper
 
         $select->columns(array('id', 'name', 'next_name', 'short_name', 'logo', 'describe', 'website', 'programme', 'background', 'phone', 'contact', 'address_id'))
                ->join(array('school_user' => 'user'), 'school_user.id=school.contact_id', array('id', 'firstname', 'lastname', 'status', 'email', 'birth_date', 'position', 'interest', 'avatar'), $select::JOIN_LEFT)
-               ->join(array('school_address' => 'address'), 'school.address_id = school_address.id', array('id', 'street_no', 'street_type', 'street_name', 'floor', 'door', 'apartment', 'building', 'longitude', 'latitude', 'timezone'),  $select::JOIN_LEFT)
-               ->join(array('school_address_division' => 'division'), 'school_address_division.id=school_address.division_id', array('id', 'name'),  $select::JOIN_LEFT)
-               ->join(array('school_address_city' => 'city'), 'school_address_city.id=school_address.city_id', array('id', 'name'),  $select::JOIN_LEFT)
-               ->join(array('school_address_country' => 'country'), 'school_address_country.id=school_address.country_id', array('id', 'short_name', 'name'),  $select::JOIN_LEFT)
+               ->join(array('school_address' => 'address'), 'school.address_id = school_address.id', array('school_address!id' => 'id', 'street_no', 'street_type', 'street_name', 'floor', 'door', 'apartment', 'building', 'longitude', 'latitude', 'timezone'),  $select::JOIN_LEFT)
+               ->join(array('school_address_division' => 'division'), 'school_address_division.id=school_address.division_id', array('school_address_division!id' => 'id', 'name'),  $select::JOIN_LEFT)
+               ->join(array('school_address_city' => 'city'), 'school_address_city.id=school_address.city_id', array('school_address_city!id' => 'id', 'name'),  $select::JOIN_LEFT)
+               ->join(array('school_address_country' => 'country'), 'school_address_country.id=school_address.country_id', array('school_address_country!id' => 'id', 'short_name', 'name'),  $select::JOIN_LEFT)
                ->where(array('school.id' => $school))
                ->where(array('school.deleted_date IS NULL'));
 
@@ -40,10 +40,10 @@ class School extends AbstractMapper
     {
         $select = $this->tableGateway->getSql()->select();
         $select->columns(array('id', 'name', 'next_name', 'short_name', 'logo', 'describe', 'website', 'programme', 'background', 'phone'))
-               ->join(array('school_address' => 'address'), 'school.address_id = school_address.id', array('id', 'street_no', 'street_type', 'street_name', 'floor', 'door', 'apartment', 'building', 'longitude', 'latitude', 'timezone'),  $select::JOIN_LEFT)
-               ->join(array('school_address_division' => 'division'), 'school_address_division.id=school_address.division_id', array('id', 'name'),  $select::JOIN_LEFT)
-               ->join(array('school_address_city' => 'city'), 'school_address_city.id=school_address.city_id', array('id', 'name'),  $select::JOIN_LEFT)
-               ->join(array('school_address_country' => 'country'), 'school_address_country.id=school_address.country_id', array('id', 'short_name', 'name'),  $select::JOIN_LEFT);
+               ->join(array('school_address' => 'address'), 'school.address_id = school_address.id', array('school_address!id' => 'id', 'street_no', 'street_type', 'street_name', 'floor', 'door', 'apartment', 'building', 'longitude', 'latitude', 'timezone'),  $select::JOIN_LEFT)
+               ->join(array('school_address_division' => 'division'), 'school_address_division.id=school_address.division_id', array('school_address_division!id' => 'id', 'name'),  $select::JOIN_LEFT)
+               ->join(array('school_address_city' => 'city'), 'school_address_city.id=school_address.city_id', array('school_address_city!id' => 'id', 'name'),  $select::JOIN_LEFT)
+               ->join(array('school_address_country' => 'country'), 'school_address_country.id=school_address.country_id', array('school_address_country!id' => 'id', 'short_name', 'name'),  $select::JOIN_LEFT);
 
         if (!empty($search)) {
             $select->where(array('(school.name LIKE ? ' => '%'.$search.'%'))
