1e1f0fe6ce57775854ccb89aa69faa3ecfa9d100
Christophe Robert <christophe.robert@thestudnet.com>
2016-01-14 15:31:25 +0100

    arthur

--- data/db/02-dataset-user.sql
+++ data/db/02-dataset-user.sql
@@ -4,3 +4,4 @@ INSERT INTO `user` (`firstname`, `lastname`, `email`, `password`) VALUES ('Chris
 INSERT INTO `user` (`firstname`, `lastname`, `email`, `password`) VALUES ('Salim', 'Bendacha', 'sbendacha@thestudnet.com', '4ac91ac4cb1614b368e3dff3ac718f1d');
 INSERT INTO `user` (`firstname`, `lastname`, `email`, `password`) VALUES ('Sébastien', 'Sayegh', 'ssayegh@thestudnet.com', '4ac91ac4cb1614b368e3dff3ac718f1d');
 INSERT INTO `user` (`firstname`, `lastname`, `email`, `password`) VALUES ('Guillaume', 'Masmejean', 'gmasmejean@thestudnet.com', '4ac91ac4cb1614b368e3dff3ac718f1d');
+INSERT INTO `user` (`firstname`, `lastname`, `email`, `password`) VALUES ('Arthur', 'Flachs', 'aflachs@thestudnet.com', '4ac91ac4cb1614b368e3dff3ac718f1d');
