8245c4596449cd91b7ebf94577583821da734b23
Christophe Robert <christophe.robert@thestudnet.com>
2016-01-14 15:59:03 +0100

    role arthur

--- data/db/11-addr-user-role.sql
+++ data/db/11-addr-user-role.sql
@@ -4,4 +4,5 @@ INSERT INTO `user_role` (`user_id`, `role_id`) VALUES ('3', '1');
 INSERT INTO `user_role` (`user_id`, `role_id`) VALUES ('4', '2');
 INSERT INTO `user_role` (`user_id`, `role_id`) VALUES ('5', '1');
 INSERT INTO `user_role` (`user_id`, `role_id`) VALUES ('6', '2');
+INSERT INTO `user_role` (`user_id`, `role_id`) VALUES ('7', '1');
 
