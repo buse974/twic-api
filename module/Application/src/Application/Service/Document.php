d83bbb87129c3ed5facf449ffcae419f5a21b7bf
Christophe Robert <crobert@thestudnet.com>
2016-03-24 15:11:02 +0100

    document

--- module/Application/src/Application/Service/Document.php
+++ module/Application/src/Application/Service/Document.php
@@ -19,10 +19,10 @@ class Document extends AbstractService
      */
     public function add($name = null, $type = null, $link = null, $token = null, $item_id = null)
     {
-        $library_id = $this->getServiceLibrary()->add($name, $link, $token, $type);
+        $library_id = $this->getServiceLibrary()->add($name, $link, $token, $type)->getId();
         $m_document = $this->getModel()
             ->setItemId($item_id)
-            ->setLibrary($library_id)
+            ->setLibraryId($library_id)
             ->setCreatedDate((new \DateTime('now', new \DateTimeZone('UTC')))->format('Y-m-d H:i:s'));
         
         if ($this->getMapper()->insert($m_document) <= 0) {
