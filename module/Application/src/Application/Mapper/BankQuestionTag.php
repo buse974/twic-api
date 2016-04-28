ff85ac7db95a37a080c3cf3c7a50c117f3e07a1f
Christophe Robert <christophe.robert@babblecar.com>
2016-04-28 13:57:53 +0200

    library bug

--- module/Application/src/Application/Mapper/BankQuestionTag.php
+++ module/Application/src/Application/Mapper/BankQuestionTag.php
@@ -9,7 +9,7 @@ class BankQuestionTag extends AbstractMapper
     public function getList($bank_question_id = null, $course_id = null, $search = null)
     {
         $select = $this->tableGateway->getSql()->select();
-        $select->columns(['name', 'bank_question_id']);
+        $select->columns(['name']);
         
         if(null !== $bank_question_id) {
             $select->where(['bank_question_tag.bank_question_id' => $bank_question_id]);
@@ -23,6 +23,8 @@ class BankQuestionTag extends AbstractMapper
             $select->join('bank_question', 'bank_question.id=bank_question_tag.bank_question_id', [])
                 ->where(['bank_question.course_id' => $course_id]);
         }
+        
+        $select->quantifier('DISTINCT');
         
         return $this->selectWith($select);
     }
