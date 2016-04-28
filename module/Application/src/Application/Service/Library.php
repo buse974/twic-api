46cb8af8bb43dc7c98d54b6eab990bed76ee5592
Christophe Robert <christophe.robert@babblecar.com>
2016-04-28 13:50:30 +0200

    library bug

--- module/Application/src/Application/Service/Library.php
+++ module/Application/src/Application/Service/Library.php
@@ -24,7 +24,7 @@ class Library extends AbstractService
 	    $urldms = $this->getServiceLocator()->get('config')['app-conf']['urldms'];
 	    
 	    $box_id = null;
-	    if($res_box = $this->getServiceBox()->addFile(($link)?:$urldms.$token, $type) !== null) {
+	    if(($res_box = $this->getServiceBox()->addFile(($link)?:$urldms.$token, $type)) !== null) {
 	        $box_id = $res_box->getId();
 	    }
 	    
