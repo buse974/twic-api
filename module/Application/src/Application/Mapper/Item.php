4de25390b6bd4d083867de6d33245b028a1b1bd5
Paul BOUSSEKEY <pboussekey@thestudnet.com>
2016-05-04 09:49:10 +0200

    Fix item.get for documents

--- module/Application/src/Application/Mapper/Item.php
+++ module/Application/src/Application/Mapper/Item.php
@@ -254,4 +254,44 @@ class Item extends AbstractMapper
 
         return $this->selectWith($select);
     }
+    
+    
+    
+    /**
+     * @param array $me
+     */
+    public function getListForCalendar($me, $start = null, $end = null)
+    {
+      $select = $this->tableGateway->getSql()->select();
+      $select->columns(['id', 'title', 'type', 'course_id', 'grading_policy_id', 'parent_id', 'order_id', 'has_submission', 'set_id',
+            'item$start' => new Expression('DATE_FORMAT(item.start, "%Y-%m-%dT%TZ")'),
+            'item$end' => new Expression('DATE_FORMAT(item.end, "%Y-%m-%dT%TZ")'),
+            'item$cut_off' => new Expression('DATE_FORMAT(item.cut_off, "%Y-%m-%dT%TZ")')
+        ])
+            ->join('course', 'course.id=item.course_id', ['id', 'title'])
+            ->join('program', 'program.id=course.program_id', ['id', 'name'])
+            ->where(array('program.school_id' => $me['school']['id']))
+            ->where('item.title IS NOT NULL')
+            ->where('item.start IS NOT NULL');
+        if (!array_key_exists(ModelRole::ROLE_ACADEMIC_ID, $me['roles'])) {
+            $select->join('course_user_relation','course.id = course_user_relation.course_id', [])
+                    ->where(array('course_user_relation.user_id' => $me['id']));
+        }
+        
+        if (array_key_exists(ModelRole::ROLE_STUDENT_ID, $me['roles'])) {
+            $select->join('set','set.id = item.set_id', [], $select::JOIN_LEFT)
+                   ->join('set_group','set.id = set_group.set_id', [], $select::JOIN_LEFT)
+                   ->join('group','group.id = set_group.group_id', [], $select::JOIN_LEFT)
+                   ->join('group_user','group.id = group_user.group_id', [], $select::JOIN_LEFT)
+                ->where('( item.set_id IS NULL ')
+                ->where(['group_user.user_id = ? )' => $me['id']], Predicate::OP_OR);
+        }
+        if (null != $start && null !== $end) {
+            $select->where(['( item.start BETWEEN ? AND ? ' => [$start,$end]])
+                ->where(['item.end BETWEEN ? AND ?  ' => [$start,$end]], Predicate::OP_OR)
+                ->where(['( item.start < ? AND item.end > ? ) ) ' => [$start,$end]], Predicate::OP_OR);
+        }
+        syslog(1, $this->printSql($select));
+        return $this->selectWith($select);
+    }
 }
