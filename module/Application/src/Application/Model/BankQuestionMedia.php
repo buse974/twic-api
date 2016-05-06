816786ef8d81b3a6951d7ac4f5b54ba0d5961331
Christophe Robert <crobert@thestudnet.com>
2016-05-06 15:06:09 +0200

    db update

--- module/Application/src/Application/Model/BankQuestionMedia.php
+++ module/Application/src/Application/Model/BankQuestionMedia.php
@@ -6,4 +6,24 @@ use Application\Model\Base\BankQuestionMedia as BaseBankQuestionMedia;
 
 class BankQuestionMedia extends BaseBankQuestionMedia
 {
+    protected $library;
+      
+    public function exchangeArray(array &$data)
+    {
+        parent::exchangeArray($data);
+    
+        $this->library = $this->requireModel('app_model_library', $data);
+    }
+    
+    public function getLibrary() 
+    {
+        return $this->library;
+    }
+    
+    public function setLibrary($library) 
+    {
+        $this->library = $library;
+        
+        return $this;
+    }
 }
