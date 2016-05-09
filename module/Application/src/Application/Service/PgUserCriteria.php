f4f9775698cf0de0a6a00c1c0d5730f70e7054e7
Paul BOUSSEKEY <pboussekey@thestudnet.com>
2016-05-09 14:18:39 +0200

    Starting submission.pairRates

--- module/Application/src/Application/Service/PgUserCriteria.php
+++ module/Application/src/Application/Service/PgUserCriteria.php
@@ -6,4 +6,27 @@ use Dal\Service\AbstractService;
 
 class PgUserCriteria extends AbstractService
 {
+    
+    /**
+     *
+     * @param integer $user
+     * @param integer $criteria
+     * @param integer $points
+     */
+    public function add($user, $submission, $criteria, $points)
+    {
+        return  $this->getMapper()->insert($this->getModel()->setUserId($user)->setSubmissionId($submission)->setCriteriaId($criteria)->setPoints($points));
+    }
+    
+    
+     /**
+     *
+     * @param integer $user
+     * @param integer $submission
+     */
+    public function deleteByUserAndSubmission($user, $submission)
+    {
+        return  $this->getMapper()->delete($this->getModel()->setUserId($user)->setSubmissionId($submission));
+    }
+    
 }
