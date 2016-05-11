54efc8b9fa624989a05d6ed68e5a0b3af020270f
Paul BOUSSEKEY <pboussekey@thestudnet.com>
2016-05-11 09:47:14 +0200

    Submission add comment

--- module/Application/src/Application/Mapper/SubmissionComments.php
+++ module/Application/src/Application/Mapper/SubmissionComments.php
@@ -6,4 +6,17 @@ use Dal\Mapper\AbstractMapper;
 
 class SubmissionComments extends AbstractMapper
 {
+    
+    /* 
+     * @param integer $id
+     * 
+     * @return \Application\Model\SubmissionComments
+     */
+    public function get($id)
+    {
+        $select = $this->tableGateway->getSql()->select();
+        $select->join('user','submission_comments.user_id = user.id', ['id', 'firstname', 'lastname', 'avatar'])
+                ->where(['submission_comments.id' => $id]);
+        return $this->selectWith($select);
+    }
 }
--- module/Application/src/Application/Model/SubmissionComments.php
+++ module/Application/src/Application/Model/SubmissionComments.php
@@ -6,4 +6,25 @@ use Application\Model\Base\SubmissionComments as BaseSubmissionComments;
 
 class SubmissionComments extends BaseSubmissionComments
 {
+    protected $user;
+      
+    public function exchangeArray(array &$data)
+    {
+        parent::exchangeArray($data);
+    
+        $this->user = $this->requireModel('app_model_user', $data);
+    }
+    
+    public function getUsers() 
+    {
+        return $this->user;
+    }
+    
+    public function setUsers($user) 
+    {
+        $this->user = $user;
+        
+        return $this;
+    }
+      
 }
