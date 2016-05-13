1b91233c66debb1c0f59ddfadee8f00fe9d076f9
Paul BOUSSEKEY <pboussekey@thestudnet.com>
2016-05-13 15:16:46 +0200

    Submission_user : set start date

---
+++ data/db/43-right.sql
@@ -0,0 +1,7 @@
+INSERT IGNORE INTO `permission` (`libelle`) VALUES ('questionnaire.getByItem');
+INSERT IGNORE INTO `role_permission` (`role_id`, `permission_id`) VALUES (4, 
+(SELECT `id` FROM `permission` WHERE `libelle`= 'questionnaire.getByItem'));
+
+INSERT IGNORE INTO `permission` (`libelle`) VALUES ('submissionuser.start');
+INSERT IGNORE INTO `role_permission` (`role_id`, `permission_id`) VALUES (4, 
+(SELECT `id` FROM `permission` WHERE `libelle`= 'submissionuser.start'));
