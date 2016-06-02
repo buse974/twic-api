cf9bd8fc9502292abf1c02344de7e377617e21db
Christophe Robert <crobert@thestudnet.com>
2016-06-02 16:59:37 +0200

    date

--- tests/Module/Api/Service/GroupWorkTest.php
+++ tests/Module/Api/Service/GroupWorkTest.php
@@ -809,7 +809,6 @@ class GroupWorkTest extends AbstractService
         $this->assertEquals($data['id'] , 1);
         $this->assertEquals($data['jsonrpc'] , 2.0);
     }
-    
 
     /**
      * @depends testCanGetSubmission
