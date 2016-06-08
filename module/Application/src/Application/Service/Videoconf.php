9bbb5d94c8c26ebb230473fe6ded565c3f569c3d
Christophe Robert <crobert@thestudnet.com>
2016-06-08 11:57:31 +0200

    right

--- module/Application/src/Application/Service/Videoconf.php
+++ module/Application/src/Application/Service/Videoconf.php
@@ -473,7 +473,8 @@ class Videoconf extends AbstractService
      * @param integer $course_id
      * @param integer $item_id
      */
-    public function getListId($program_id = null, $course_id = null, $item_id = null){
+    public function getListId($program_id = null, $course_id = null, $item_id = null)
+    {
        $me = $this->getServiceUser()->getIdentity();
        $res_videoconf = $this->getMapper()->getListId($me['school']['id'], $program_id, $course_id, $item_id);
        $ids = [];
