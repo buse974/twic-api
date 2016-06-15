a355fb7192343a9ecc165a5c1e6b90e2ce8f4649
Christophe Robert <christophe.robert@babblecar.com>
2016-06-15 17:13:13 +0200

    videoconf

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -57,8 +57,6 @@ return [
             'app_service_message',
             'app_service_module_assignments',
             'app_service_item_assignment',
-            'app_service_grading_policy_grade',
-            'app_service_grading_policy_grade_comment',
             'app_service_item_assignment_comment',
             'app_service_task',
             'app_service_task_share',
