a17e49a20d1a17085fc1c48958e4e99103918fd4
Christophe Robert <crobert@thestudnet.com>
2016-06-21 17:14:56 +0200

    right

--- data/db/74-right.sql
+++
@@ -1,3 +0,0 @@
-INSERT IGNORE INTO `permission` (`libelle`) VALUES ('conversation.get');
-INSERT IGNORE INTO `role_permission` (`role_id`, `permission_id`) VALUES (5, 
-(SELECT `id` FROM `permission` WHERE `libelle`= 'conversation.get'));
