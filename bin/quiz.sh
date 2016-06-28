1434ed6fe3ff99a728e303391ddb12aaba41ac87
Christophe Robert <crobert@thestudnet.com>
2016-06-28 19:11:44 +0200

    quiz

---
+++ bin/quiz.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+while getopts "p::u::a::hx:" option
+do
+        case $option in
+                a)
+                        URL_API=$OPTARG
+                ;;
+		h)
+			H=true
+		;;
+        esac
+done
+
+if [ -z "$URL_API" ] || [ ! -z "$H" ] ; then
+ 	echo -e "\n\n-----------------------------------------------------------------------------------";
+	echo -e "| uploadZopentok.sh -a -h                                                         |";
+        echo -e "| -a <url_api> : Url api                                                          |";
+        echo -e "| -h : help                                                                       |";
+        echo -e "*---------------------------------------------------------------------------------*\n\n";
+exit 1
+fi
+
+curl --data-binary '{"jsonrpc": "2.0", "id":1, "method": "subquiz.checkGrade", "params": [] }' $URL_API
