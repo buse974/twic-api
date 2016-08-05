aea195581638a416ca5ee0d360ee72c1e728fd7a
Christophe Robert <crobert@thestudnet.com>
2016-08-05 16:15:38 +0200

    doc + db 1238

--- data/dbtest/128-heritage.sql
+++ data/dbtest/128-heritage.sql
@@ -16,44 +16,44 @@ CREATE TABLE IF NOT EXISTS `organization_user` (
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
-CREATE TABLE IF NOT EXISTS `inv`.`organization_relation` (
+CREATE TABLE IF NOT EXISTS `organization_relation` (
   `organization_id` INT UNSIGNED NOT NULL,
   `parent_id` INT UNSIGNED NOT NULL,
   PRIMARY KEY (`organization_id`, `parent_id`),
   INDEX `fk_organization_relation_2_idx` (`parent_id` ASC),
   CONSTRAINT `fk_organization_relation_1`
     FOREIGN KEY (`organization_id`)
-    REFERENCES `inv`.`school` (`id`)
+    REFERENCES `school` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_organization_relation_2`
     FOREIGN KEY (`parent_id`)
-    REFERENCES `inv`.`school` (`id`)
+    REFERENCES `school` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
-CREATE TABLE IF NOT EXISTS `inv`.`circle` (
+CREATE TABLE IF NOT EXISTS `circle` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
-CREATE TABLE IF NOT EXISTS `inv`.`circle_organization` (
+CREATE TABLE IF NOT EXISTS `circle_organization` (
   `circle_id` INT UNSIGNED NOT NULL,
   `organization_id` INT UNSIGNED NOT NULL,
   PRIMARY KEY (`circle_id`, `organization_id`),
   INDEX `fk_circle_organization_2_idx` (`organization_id` ASC),
   CONSTRAINT `fk_circle_organization_1`
     FOREIGN KEY (`circle_id`)
-    REFERENCES `inv`.`circle` (`id`)
+    REFERENCES `circle` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_circle_organization_2`
     FOREIGN KEY (`organization_id`)
-    REFERENCES `inv`.`school` (`id`)
+    REFERENCES `school` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
