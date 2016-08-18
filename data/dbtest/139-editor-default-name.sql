6443e61de11d2bac6f7bfd0b656636e3810a0176
Arthur Flachs <aflachs@thestudnet.com>
2016-08-18 16:20:09 +0200

    Test

--- data/dbtest/139-editor-default-name.sql
+++ data/dbtest/139-editor-default-name.sql
@@ -1,2 +1,1 @@
-ALTER TABLE `text_editor` 
-    CHANGE COLUMN `name` VARCHAR(255) NOT NULL DEFAULT 'Document 1';
+ALTER TABLE `text_editor` CHANGE COLUMN name VARCHAR(255) NOT NULL DEFAULT 'Document 1';
