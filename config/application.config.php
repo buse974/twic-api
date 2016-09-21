4af3452a9a51fdf641489f57fc059a827730018a
Christophe Robert <crobert@thestudnet.com>
2016-09-21 10:01:12 +0200

    config local.php

--- config/application.config.php
+++ config/application.config.php
@@ -1,8 +1,8 @@
 <?php
 
-return array(
+return [
     // This should be an array of module namespaces used in the application.
-    'modules' => array(
+    'modules' => [
         'Application',
         'Box',
         'Dal',
@@ -16,15 +16,15 @@ return array(
         'Zend\I18n',
         'Zend\Mvc\I18n',
         'SimplePageCrawler',
-    ),
+    ],
 
     // These are various options for the listeners attached to the ModuleManager
-    'module_listener_options' => array(
+    'module_listener_options' => [
         // This should be an array of paths in which modules reside.
         // If a string key is provided, the listener will consider that a module
         // namespace, the value of that key the specific path to that module's
         // Module class.
-        'module_paths' => array(
+        'module_paths' => [
             'Dal' => __DIR__.'/../vendor/buse974/dal',
             'JRpc' => __DIR__.'/../vendor/buse974/jrpc',
             'Address' => __DIR__.'/../vendor/buse974/address',
@@ -35,15 +35,15 @@ return array(
             'Rbac' => __DIR__.'/../module/Rbac',
             'Box' => __DIR__.'/../module/Box',
             'SimplePageCrawler' => __DIR__.'/../vendor/blanchonvincent/simple-page-crawler',
-        ),
+        ],
 
         // An array of paths from which to glob configuration files after
         // modules are loaded. These effectively override configuration
         // provided by modules themselves. Paths may use GLOB_BRACE notation.
-        'config_glob_paths' => array(
+        'config_glob_paths' => [
             'config/autoload/global.php',
             'config/autoload/local.php',
-        ),
+        ],
 
         // Whether or not to enable a configuration cache.
         // If enabled, the merged configuration will be cached and used in
@@ -68,11 +68,11 @@ return array(
         // Enabled by default, prevents usage of modules that depend on other modules
         // that weren't loaded.
          'check_dependencies' => false,
-    ),
+    ],
 
     // Used to create an own service manager. May contain one or more child arrays.
-    //'service_listener_options' => array(
-    //     array(
+    //'service_listener_options' => [
+    //     [
     //         'service_manager' => $stringServiceManagerName,
     //         'config_key'      => $stringConfigKey,
     //         'interface'       => $stringOptionalInterface,
@@ -82,5 +82,5 @@ return array(
 
    // Initial configuration with which to seed the ServiceManager.
    // Should be compatible with Zend\ServiceManager\Config.
-   // 'service_manager' => array(),
-);
+   // 'service_manager' => [),
+];
--- config/autoload/global.php
+++ config/autoload/global.php
@@ -14,7 +14,7 @@
 
 return [
     'version' => "v1.2",
-    'build-commit' => 3,
+    'build-commit' => 4,
     'app-conf' => [
         'cache' => 'storage_memcached',
         'secret_key' => 'toto',
@@ -155,13 +155,9 @@ return [
             'adapter' => [
                 'name' => 'memcached',
                 'options' => [
-                    'servers' => [
-                        ['host' => 'localhost', 'port' => '11211', 'weight' => 66],
-                        ['host' => 'localhost', 'port' => '11211', 'weight' => 33],
-                    ],
-                    'namespace' => 'LMS3',
+                    'namespace' => 'LMS4',
                     'liboptions' => [
-                        ['option' => Memcached::OPT_PREFIX_KEY, 'value' => 'LMS3'],
+                        ['option' => Memcached::OPT_PREFIX_KEY, 'value' => 'LMS4'],
                         ['option' => Memcached::OPT_LIBKETAMA_COMPATIBLE, 'value' => true],
                         ['option' => Memcached::OPT_SERIALIZER, 'value' => Memcached::SERIALIZER_IGBINARY],
                         ['option' => Memcached::OPT_DISTRIBUTION, 'value' => Memcached::DISTRIBUTION_CONSISTENT],
