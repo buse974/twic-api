6db6b285fa7e80c4526ddde1256abab786c567ca
Christophe Robert <crobert@thestudnet.com>
2016-10-07 15:39:24 +0200

    bug cascade

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -14,7 +14,7 @@
 
 return [
     'version' => "v1.2",
-    'build-commit' => 73,
+    'build-commit' => 74,
     'app-conf' => [
         'cache' => 'storage_memcached',
         'secret_key' => 'toto',
@@ -157,9 +157,9 @@ return [
             'adapter' => [
                 'name' => 'memcached',
                 'options' => [
-                    'namespace' => 'LMS73',
+                    'namespace' => 'LMS74',
                     'liboptions' => [
-                        ['option' => Memcached::OPT_PREFIX_KEY, 'value' => 'LMS73'],
+                        ['option' => Memcached::OPT_PREFIX_KEY, 'value' => 'LMS74'],
                         ['option' => Memcached::OPT_LIBKETAMA_COMPATIBLE, 'value' => true],
                         ['option' => Memcached::OPT_SERIALIZER, 'value' => Memcached::SERIALIZER_IGBINARY],
                         ['option' => Memcached::OPT_DISTRIBUTION, 'value' => Memcached::DISTRIBUTION_CONSISTENT],
