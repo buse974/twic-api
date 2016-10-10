00ca0c0f50f76e61b338ab5cf271d34107c6a582
Christophe Robert <crobert@thestudnet.com>
2016-10-10 12:24:42 +0200

    bug: library id

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -14,7 +14,7 @@
 
 return [
     'version' => "v1.2",
-    'build-commit' => 82,
+    'build-commit' => 84,
     'app-conf' => [
         'cache' => 'storage_memcached',
         'secret_key' => 'toto',
@@ -157,9 +157,9 @@ return [
             'adapter' => [
                 'name' => 'memcached',
                 'options' => [
-                    'namespace' => 'LMS82',
+                    'namespace' => 'LMS84',
                     'liboptions' => [
-                        ['option' => Memcached::OPT_PREFIX_KEY, 'value' => 'LMS82'],
+                        ['option' => Memcached::OPT_PREFIX_KEY, 'value' => 'LMS84'],
                         ['option' => Memcached::OPT_LIBKETAMA_COMPATIBLE, 'value' => true],
                         ['option' => Memcached::OPT_SERIALIZER, 'value' => Memcached::SERIALIZER_IGBINARY],
                         ['option' => Memcached::OPT_DISTRIBUTION, 'value' => Memcached::DISTRIBUTION_CONSISTENT],
