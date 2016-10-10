960fd4482fd9cc14b466948977cee14f0f610b1d
Christophe Robert <crobert@thestudnet.com>
2016-10-10 11:01:19 +0200

    message doc

--- module/Application/src/Application/Service/MessageDoc.php
+++ module/Application/src/Application/Service/MessageDoc.php
@@ -7,6 +7,7 @@
 namespace Application\Service;
 
 use Dal\Service\AbstractService;
+use Application\Model\Library as ModelLibrary;
 
 /**
  * Class MessageDoc.
@@ -33,9 +34,13 @@ class MessageDoc extends AbstractService
             $this->getMapper()->delete($m_message_doc);
             $m_message_doc->setCreatedDate((new \DateTime('now', new \DateTimeZone('UTC')))->format('Y-m-d H:i:s'));
             foreach ($document as $d) {
+                
                 $token = (isset($d['token'])) ? $d['token']:$d;
                 $name = (isset($d['name'])) ? $d['name']:null;  
-                $m_message_doc->setToken($token)->setName($name);
+                $type = (isset($d['type'])) ? $d['type']:null;
+                
+                $library_id = $this->getServiceLibrary()->add($name,null,$token,$type,ModelLibrary::FOLDER_OTHER_INT);
+                $m_message_doc->setToken($token)->setName($name)->setLibrary($library_id);
                 $ret[$token] = $this->getMapper()->insert($m_message_doc);
             }
         }
@@ -54,5 +59,15 @@ class MessageDoc extends AbstractService
     {
         return $this->getMapper()->select($this->getModel()
             ->setMessageId($message_id));
+    }
+    
+    /**
+     * Get Service Library
+     * 
+     * @return \Application\Service\Library
+     */
+    private function getServiceLibrary()
+    {
+        return $this->container->get('app_service_library');
     }
 }
