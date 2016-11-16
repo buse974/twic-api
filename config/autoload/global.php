701cf4fa5bf4873434759628479ad3fa9491a41b
Christophe Robert <crobert@thestudnet.com>
2016-11-16 17:35:26 +0100

    course

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -14,7 +14,7 @@
 
 return [
     'version' => "v1.2",
-    'build-commit' => 180,
+    'build-commit' => 181,
     'app-conf' => [
         'cache' => 'storage_memcached',
         'secret_key' => 'toto',
@@ -162,9 +162,9 @@ return [
             'adapter' => [
                 'name' => 'memcached',
                 'options' => [
-                    'namespace' => 'LMS180',
+                    'namespace' => 'LMS181',
                     'liboptions' => [
-                        ['option' => Memcached::OPT_PREFIX_KEY, 'value' => 'LMS180'],
+                        ['option' => Memcached::OPT_PREFIX_KEY, 'value' => 'LMS181'],
                         ['option' => Memcached::OPT_LIBKETAMA_COMPATIBLE, 'value' => true],
                         ['option' => Memcached::OPT_SERIALIZER, 'value' => Memcached::SERIALIZER_IGBINARY],
                         ['option' => Memcached::OPT_DISTRIBUTION, 'value' => Memcached::DISTRIBUTION_CONSISTENT],
--- module/Application/src/Application/Service/Post.php
+++ module/Application/src/Application/Service/Post.php
@@ -189,7 +189,11 @@ class Post extends AbstractService
             $sub = [$sub];
         }
 
-        return $this->add($content, null,null,null,null,null,$parent_id,$t_page_id,$t_organization_id,$t_user_id,$t_course_id,null,null,null,null,null, 
+        $res_post = $this->getMapper()->select($this->getModel()->setUid($uid));
+        
+        return ($res_post->count() > 0) ?
+            $this->updateSys($uid, $content, $data, $event) :
+            $this->add($content, null,null,null,null,null,$parent_id,$t_page_id,$t_organization_id,$t_user_id,$t_course_id,null,null,null,null,null, 
             $data, $event, $uid, $sub, $type);
     }
     
