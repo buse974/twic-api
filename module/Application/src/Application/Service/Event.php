22f609bbfc0ef9f7c3c99351213c6016f9174b71
Christophe Robert <crobert@thestudnet.com>
2016-11-22 11:52:59 +0100

    post cascade

--- module/Application/src/Application/Service/Event.php
+++ module/Application/src/Application/Service/Event.php
@@ -212,7 +212,7 @@ class Event extends AbstractService
         } else {
             $event = $type;
             if(null !== $ev) {
-                $event = '.'.$ev;
+                $event .= '.'.$ev;
             }
         }
         
