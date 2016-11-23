4e7dd0a19139fd188d3c3600c92f00fbf494899c
Christophe Robert <crobert@thestudnet.com>
2016-11-23 17:53:39 +0100

    post

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -14,7 +14,7 @@
 
 return [
     'version' => "v1.2",
-    'build-commit' => 260,
+    'build-commit' => 261,
     'app-conf' => [
         'cache' => 'storage_memcached',
         'secret_key' => 'toto',
@@ -162,9 +162,9 @@ return [
             'adapter' => [
                 'name' => 'memcached',
                 'options' => [
-                    'namespace' => 'LMS260',
+                    'namespace' => 'LMS261',
                     'liboptions' => [
-                        ['option' => Memcached::OPT_PREFIX_KEY, 'value' => 'LMS260'],
+                        ['option' => Memcached::OPT_PREFIX_KEY, 'value' => 'LMS261'],
                         ['option' => Memcached::OPT_LIBKETAMA_COMPATIBLE, 'value' => true],
                         ['option' => Memcached::OPT_SERIALIZER, 'value' => Memcached::SERIALIZER_IGBINARY],
                         ['option' => Memcached::OPT_DISTRIBUTION, 'value' => Memcached::DISTRIBUTION_CONSISTENT],
--- module/Application/src/Application/Mapper/Post.php
+++ module/Application/src/Application/Mapper/Post.php
@@ -79,7 +79,7 @@ class Post extends AbstractMapper
             }
         }
         
-        $select->join('user','user.id = post.user_id',['id', 'firstname', 'lastname', 'nickname', 'avatar', 'ambassador'])
+        $select->join('user','user.id = post.user_id',['id', 'firstname', 'lastname', 'nickname', 'avatar', 'ambassador'], $select::JOIN_LEFT)
                 ->join('school','user.school_id = school.id',['id', 'short_name', 'logo'])
                 ->where(['post.deleted_date IS NULL'])
                 ->order(['post.id' => 'DESC']);
@@ -163,7 +163,7 @@ class Post extends AbstractMapper
         ]);
         
         if (!$for_mobile) {
-            $select->join('user','user.id = post.user_id',['id', 'firstname', 'lastname', 'nickname', 'avatar', 'ambassador'])
+            $select->join('user','user.id = post.user_id',['id', 'firstname', 'lastname', 'nickname', 'avatar', 'ambassador'], $select::JOIN_LEFT)
                 ->join('school','user.school_id = school.id',['id', 'short_name', 'logo']);   
         }
         $select->where(['post.id' => $id])
