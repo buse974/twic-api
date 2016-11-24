734a2290b6deeb134ad69fd1642d11e0dede866e
Christophe Robert <crobert@thestudnet.com>
2016-11-24 15:25:45 +0100

    sub

--- data/dbtest/195-sub.sql
+++ data/dbtest/195-sub.sql
@@ -1,6 +1,7 @@
-INSERT IGNORE INTO `subscription`
-(`libelle`,
-`user_id`)
-SELECT CONCAT('PU',contact_id), user_id FROM contact
+INSERT IGNORE INTO `apilms`.`subscription`
+(`libelle`, `user_id`, `created_date`) 
+SELECT CONCAT('PU',contact.contact_id), contact.user_id, UTC_TIMESTAMP() FROM contact
+LEFT JOIN subscription ON subscription.user_id=contact.user_id AND subscription.libelle=CONCAT('PU',contact.contact_id) 
 WHERE contact.accepted_date IS NOT NULL 
-AND contact.deleted_date IS NULL
+AND subscription.user_id IS NULL
+AND contact.deleted_date IS NULL;
