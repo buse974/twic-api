f9f022652dfd768dd2f0983198141faff8551538
Christophe Robert <crobert@thestudnet.com>
2016-12-19 09:35:56 +0100

    bug + doc + cs

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -14,7 +14,7 @@
 
 return [
     'version' => "v1.2",
-    'build-commit' => 282,
+    'build-commit' => 284,
     'app-conf' => [
         'cache' => 'storage_memcached',
         'secret_key' => 'toto',
@@ -162,9 +162,9 @@ return [
             'adapter' => [
                 'name' => 'memcached',
                 'options' => [
-                    'namespace' => 'LMS282',
+                    'namespace' => 'LMS284',
                     'liboptions' => [
-                        ['option' => Memcached::OPT_PREFIX_KEY, 'value' => 'LMS282'],
+                        ['option' => Memcached::OPT_PREFIX_KEY, 'value' => 'LMS284'],
                         ['option' => Memcached::OPT_LIBKETAMA_COMPATIBLE, 'value' => true],
                         ['option' => Memcached::OPT_SERIALIZER, 'value' => Memcached::SERIALIZER_IGBINARY],
                         ['option' => Memcached::OPT_DISTRIBUTION, 'value' => Memcached::DISTRIBUTION_CONSISTENT],
