37dac5f81f62bc5c623964c72861bed0a98e8bfe
Christophe Robert <crobert@thestudnet.com>
2017-01-03 17:31:02 +0100

    lost password

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -14,7 +14,7 @@
 
 return [
     'version' => "v1.3",
-    'build-commit' => 307,
+    'build-commit' => 309,
     'app-conf' => [
         'cache' => 'storage_memcached',
         'secret_key' => 'toto',
@@ -167,9 +167,9 @@ return [
             'adapter' => [
                 'name' => 'memcached',
                 'options' => [
-                    'namespace' => 'LMS307',
+                    'namespace' => 'LMS309',
                     'liboptions' => [
-                        ['option' => Memcached::OPT_PREFIX_KEY, 'value' => 'LMS307'],
+                        ['option' => Memcached::OPT_PREFIX_KEY, 'value' => 'LMS309'],
                         ['option' => Memcached::OPT_LIBKETAMA_COMPATIBLE, 'value' => true],
                         ['option' => Memcached::OPT_SERIALIZER, 'value' => Memcached::SERIALIZER_IGBINARY],
                         ['option' => Memcached::OPT_DISTRIBUTION, 'value' => Memcached::DISTRIBUTION_CONSISTENT],
