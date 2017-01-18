0a004fb260a4ea5fee04123819da576e646d1d76
Christophe Robert <crobert@thestudnet.com>
2017-01-18 11:12:41 +0100

    version

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -13,8 +13,8 @@
  */
 
 return [
-    'version' => "v1.4",
-    'build-commit' => 335,
+    'version' => "v1.5",
+    'build-commit' => 336,
     'app-conf' => [
         'cache' => 'storage_memcached',
         'secret_key' => 'toto',
@@ -167,9 +167,9 @@ return [
             'adapter' => [
                 'name' => 'memcached',
                 'options' => [
-                    'namespace' => 'LMS335',
+                    'namespace' => 'LMS336',
                     'liboptions' => [
-                        ['option' => Memcached::OPT_PREFIX_KEY, 'value' => 'LMS335'],
+                        ['option' => Memcached::OPT_PREFIX_KEY, 'value' => 'LMS336'],
                         ['option' => Memcached::OPT_LIBKETAMA_COMPATIBLE, 'value' => true],
                         ['option' => Memcached::OPT_SERIALIZER, 'value' => Memcached::SERIALIZER_IGBINARY],
                         ['option' => Memcached::OPT_DISTRIBUTION, 'value' => Memcached::DISTRIBUTION_CONSISTENT],
