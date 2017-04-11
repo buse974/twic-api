9532e9ec3b4eba87ce2985c9bde9c52140a83b36
Paul BOUSSEKEY <pboussekey@thestudnet.com>
2017-04-11 17:00:40 +0200

    Message.getList : return user_id

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -14,7 +14,7 @@
 
 return [
     'version' => "v1.5",
-    'build-commit' => 385,
+    'build-commit' => 386,
     'app-conf' => [
         'cache' => 'storage_memcached',
         'secret_key' => 'toto',
@@ -116,9 +116,9 @@ return [
             'adapter' => [
                 'name' => 'memcached',
                 'options' => [
-                    'namespace' => 'LMS385',
+                    'namespace' => 'LMS386',
                     'liboptions' => [
-                        ['option' => Memcached::OPT_PREFIX_KEY, 'value' => 'LMS385'],
+                        ['option' => Memcached::OPT_PREFIX_KEY, 'value' => 'LMS386'],
                         ['option' => Memcached::OPT_LIBKETAMA_COMPATIBLE, 'value' => true],
                         ['option' => Memcached::OPT_SERIALIZER, 'value' => Memcached::SERIALIZER_IGBINARY],
                         ['option' => Memcached::OPT_DISTRIBUTION, 'value' => Memcached::DISTRIBUTION_CONSISTENT],
--- module/Application/src/Application/Mapper/Message.php
+++ module/Application/src/Application/Mapper/Message.php
@@ -10,7 +10,7 @@ class Message extends AbstractMapper
   public function getList($user_id, $conversation_id)
   {
       $select = $this->tableGateway->getSql()->select();
-      $select->columns(['id', 'text', 'message$user_id' =>'message.user_id', 'message$created_date' => new Expression('DATE_FORMAT(message.created_date, "%Y-%m-%dT%TZ")')])
+      $select->columns(['id', 'text', 'user_id', 'message$created_date' => new Expression('DATE_FORMAT(message.created_date, "%Y-%m-%dT%TZ")')])
         ->join(['message_library' => 'library'], 'message_library.id=message.library_id', ['id', 'name', 'link', 'token', 'type', 'box_id'], $select::JOIN_LEFT)
         ->join(['message_message_user' => 'message_user'], new Expression('message.id=message_message_user.message_id AND message_message_user.user_id = ?', [$user_id]) ,  ['message_message_user$read_date' => new Expression('DATE_FORMAT(message_message_user.read_date, "%Y-%m-%dT%TZ")')], $select::JOIN_LEFT)
         ->where(['message_message_user.deleted_date IS NULL'])
