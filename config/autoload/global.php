6073fabb73343dc8a463a321a86da52499fc352c
Paul BOUSSEKEY <pboussekey@thestudnet.com>
2017-04-12 11:08:47 +0200

    Conversation script

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -14,7 +14,7 @@
 
 return [
     'version' => "v1.5",
-    'build-commit' => 391,
+    'build-commit' => 392,
     'app-conf' => [
         'cache' => 'storage_memcached',
         'secret_key' => 'toto',
@@ -116,9 +116,9 @@ return [
             'adapter' => [
                 'name' => 'memcached',
                 'options' => [
-                    'namespace' => 'LMS391',
+                    'namespace' => 'LMS392',
                     'liboptions' => [
-                        ['option' => Memcached::OPT_PREFIX_KEY, 'value' => 'LMS391'],
+                        ['option' => Memcached::OPT_PREFIX_KEY, 'value' => 'LMS392'],
                         ['option' => Memcached::OPT_LIBKETAMA_COMPATIBLE, 'value' => true],
                         ['option' => Memcached::OPT_SERIALIZER, 'value' => Memcached::SERIALIZER_IGBINARY],
                         ['option' => Memcached::OPT_DISTRIBUTION, 'value' => Memcached::DISTRIBUTION_CONSISTENT],
--- data/db/226-script-conversation.sql
+++ data/db/226-script-conversation.sql
@@ -1,4 +1,7 @@
-UPDATE message, message_user, message_doc
-SET message.user_id = message_user.from_id, message.library_id = message_doc.library_id
-WHERE message_user.message_id = message.id AND message_user.user_id = message_user.from_id
-    AND message_doc.message_id = message.id;
+UPDATE message, message_user
+SET message.user_id = message_user.from_id
+WHERE message_user.message_id = message.id AND message_user.user_id = message_user.from_id;
+
+UPDATE message,  message_doc
+SET message.library_id = message_doc.library_id
+WHERE  message_doc.message_id = message.id;
