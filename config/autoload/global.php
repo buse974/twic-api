f6e6f96776ee5d1f88d899e14c17025b49e49090
Christophe Robert <crobert@thestudnet.com>
2017-04-12 09:55:03 +0200

    message sans libray null

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -14,7 +14,7 @@
 
 return [
     'version' => "v1.5",
-    'build-commit' => 387,
+    'build-commit' => 388,
     'app-conf' => [
         'cache' => 'storage_memcached',
         'secret_key' => 'toto',
@@ -116,9 +116,9 @@ return [
             'adapter' => [
                 'name' => 'memcached',
                 'options' => [
-                    'namespace' => 'LMS387',
+                    'namespace' => 'LMS388',
                     'liboptions' => [
-                        ['option' => Memcached::OPT_PREFIX_KEY, 'value' => 'LMS387'],
+                        ['option' => Memcached::OPT_PREFIX_KEY, 'value' => 'LMS388'],
                         ['option' => Memcached::OPT_LIBKETAMA_COMPATIBLE, 'value' => true],
                         ['option' => Memcached::OPT_SERIALIZER, 'value' => Memcached::SERIALIZER_IGBINARY],
                         ['option' => Memcached::OPT_DISTRIBUTION, 'value' => Memcached::DISTRIBUTION_CONSISTENT],
