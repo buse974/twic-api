ae5e1f2323b1e1d1c8642e20b4cfeb26d9b0e98d
Christophe Robert <crobert@thestudnet.com>
2017-04-14 11:40:11 +0200

    library.delete

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -14,7 +14,7 @@
 
 return [
     'version' => "v1.5",
-    'build-commit' => 408,
+    'build-commit' => 409,
     'app-conf' => [
         'cache' => 'storage_memcached',
         'secret_key' => 'toto',
@@ -117,9 +117,9 @@ return [
             'adapter' => [
                 'name' => 'memcached',
                 'options' => [
-                    'namespace' => 'LMS408',
+                    'namespace' => 'LMS409',
                     'liboptions' => [
-                        ['option' => Memcached::OPT_PREFIX_KEY, 'value' => 'LMS408'],
+                        ['option' => Memcached::OPT_PREFIX_KEY, 'value' => 'LMS409'],
                         ['option' => Memcached::OPT_LIBKETAMA_COMPATIBLE, 'value' => true],
                         ['option' => Memcached::OPT_SERIALIZER, 'value' => Memcached::SERIALIZER_IGBINARY],
                         ['option' => Memcached::OPT_DISTRIBUTION, 'value' => Memcached::DISTRIBUTION_CONSISTENT],
