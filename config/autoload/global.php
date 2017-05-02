205037b47f4674275e8003f3cc59fd0a90b31281
Christophe Robert <crobert@thestudnet.com>
2017-05-02 14:39:14 +0200

    right library box

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -14,7 +14,7 @@
 
 return [
     'version' => "v1.5b411",
-    'build-commit' => 456,
+    'build-commit' => 457,
     'app-conf' => [
         'cache' => 'storage_memcached',
         'secret_key' => 'toto',
@@ -118,9 +118,9 @@ return [
             'adapter' => [
                 'name' => 'memcached',
                 'options' => [
-                    'namespace' => 'LMS456',
+                    'namespace' => 'LMS457',
                     'liboptions' => [
-                        ['option' => Memcached::OPT_PREFIX_KEY, 'value' => 'LMS456'],
+                        ['option' => Memcached::OPT_PREFIX_KEY, 'value' => 'LMS457'],
                         ['option' => Memcached::OPT_LIBKETAMA_COMPATIBLE, 'value' => true],
                         ['option' => Memcached::OPT_SERIALIZER, 'value' => Memcached::SERIALIZER_IGBINARY],
                         ['option' => Memcached::OPT_DISTRIBUTION, 'value' => Memcached::DISTRIBUTION_CONSISTENT],
