6f075e7b18203feafa8173733bf5b8d0f6de63f6
Christophe Robert <crobert@thestudnet.com>
2017-06-06 11:50:03 +0200

    add right circle

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -14,7 +14,7 @@
 
 return [
     'version' => "2.0.4",
-    'build-commit' => 554,
+    'build-commit' => 555,
     'app-conf' => [
         'cache' => 'storage_memcached',
         'secret_key' => 'toto',
@@ -119,9 +119,9 @@ return [
             'adapter' => [
                 'name' => 'memcached',
                 'options' => [
-                    'namespace' => 'LMS554',
+                    'namespace' => 'LMS555',
                     'liboptions' => [
-                        ['option' => Memcached::OPT_PREFIX_KEY, 'value' => 'LMS554'],
+                        ['option' => Memcached::OPT_PREFIX_KEY, 'value' => 'LMS555'],
                         ['option' => Memcached::OPT_LIBKETAMA_COMPATIBLE, 'value' => true],
                         ['option' => Memcached::OPT_SERIALIZER, 'value' => Memcached::SERIALIZER_IGBINARY],
                         ['option' => Memcached::OPT_DISTRIBUTION, 'value' => Memcached::DISTRIBUTION_CONSISTENT],
