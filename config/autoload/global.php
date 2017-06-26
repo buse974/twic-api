7a5c5ac44edc1e6697ae2c162e24ff0452eab579
Christophe Robert <crobert@thestudnet.com>
2017-06-26 12:34:38 +0200

    cong memached.host

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -14,7 +14,7 @@
 
 return [
     'version' => "2.0.4",
-    'build-commit' => 582,
+    'build-commit' => 583,
     'app-conf' => [
         'cache' => 'storage_memcached',
         'secret_key' => 'toto',
@@ -149,9 +149,9 @@ return [
             'adapter' => [
                 'name' => 'memcached',
                 'options' => [
-                    'namespace' => 'LMS582',
+                    'namespace' => 'LMS583',
                     'liboptions' => [
-                        ['option' => Memcached::OPT_PREFIX_KEY, 'value' => 'LMS582'],
+                        ['option' => Memcached::OPT_PREFIX_KEY, 'value' => 'LMS583'],
                         ['option' => Memcached::OPT_LIBKETAMA_COMPATIBLE, 'value' => true],
                         ['option' => Memcached::OPT_SERIALIZER, 'value' => Memcached::SERIALIZER_IGBINARY],
                         ['option' => Memcached::OPT_DISTRIBUTION, 'value' => Memcached::DISTRIBUTION_CONSISTENT],
