252c7b22b10686ade3ad5e2ab5e4a3c4c4bdc5a8
Paul BOUSSEKEY <pboussekey@thestudnet.com>
2017-07-11 10:45:59 +0200

    Item.getListItemUsers rights

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -14,7 +14,7 @@
 
 return [
     'version' => "2.0.4",
-    'build-commit' => 602,
+    'build-commit' => 604,
     'app-conf' => [
         'cache' => 'storage_memcached',
         'secret_key' => 'toto',
@@ -138,9 +138,9 @@ return [
             'adapter' => [
                 'name' => 'memcached',
                 'options' => [
-                    'namespace' => 'LMS602',
+                    'namespace' => 'LMS604',
                     'liboptions' => [
-                        ['option' => Memcached::OPT_PREFIX_KEY, 'value' => 'LMS602'],
+                        ['option' => Memcached::OPT_PREFIX_KEY, 'value' => 'LMS604'],
                         ['option' => Memcached::OPT_LIBKETAMA_COMPATIBLE, 'value' => true],
                         ['option' => Memcached::OPT_SERIALIZER, 'value' => Memcached::SERIALIZER_IGBINARY],
                         ['option' => Memcached::OPT_DISTRIBUTION, 'value' => Memcached::DISTRIBUTION_CONSISTENT],
