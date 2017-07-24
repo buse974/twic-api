75707b1bc1fdbbe6ea2bbc17dc956b55f089fe78
Paul BOUSSEKEY <pboussekey@thestudnet.com>
2017-07-24 09:36:51 +0200

    Fix submission.getListLibrary

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -14,7 +14,7 @@
 
 return [
     'version' => "2.0.4",
-    'build-commit' => 656,
+    'build-commit' => 657,
     'app-conf' => [
         'cache' => 'storage_memcached',
         'secret_key' => 'toto',
@@ -138,9 +138,9 @@ return [
             'adapter' => [
                 'name' => 'memcached',
                 'options' => [
-                    'namespace' => 'LMS656',
+                    'namespace' => 'LMS657',
                     'liboptions' => [
-                        ['option' => Memcached::OPT_PREFIX_KEY, 'value' => 'LMS656'],
+                        ['option' => Memcached::OPT_PREFIX_KEY, 'value' => 'LMS657'],
                         ['option' => Memcached::OPT_LIBKETAMA_COMPATIBLE, 'value' => true],
                         ['option' => Memcached::OPT_SERIALIZER, 'value' => Memcached::SERIALIZER_IGBINARY],
                         ['option' => Memcached::OPT_DISTRIBUTION, 'value' => Memcached::DISTRIBUTION_CONSISTENT],
