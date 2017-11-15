def6bbdbcde6167ea3ad691077faa4d66caad4a5
Paul BOUSSEKEY <pboussekey@thestudnet.com>
2017-11-15 16:15:03 +0100

    Fix conversation.getId

--- module/Application/src/Application/Mapper/Conversation.php
+++ module/Application/src/Application/Mapper/Conversation.php
@@ -56,7 +56,7 @@ class Conversation extends AbstractMapper
         $select = $this->tableGateway->getSql()->select();
         $select->columns($colums)
             ->join('item', 'item.conversation_id=conversation.id', ['conversation$item_id' => 'id'], $select::JOIN_LEFT)
-            ->join('page', new Expression("page.conversation_id=conversation.id OR (item.page_id=page_id AND item.participants = 'all')"), ['conversation$page_id' => 'id'], $select::JOIN_LEFT)
+            ->join('page', new Expression("page.conversation_id=conversation.id OR (item.page_id=page.id AND item.participants = 'all')"), ['conversation$page_id' => 'id'], $select::JOIN_LEFT)
             ->where(['page.deleted_date IS NULL'])
             ->where(['( ( page.type = "course" AND page.is_published IS TRUE ) OR page.type <> "course" OR page.type IS NULL )'])
             ->where(['( conversation.type <> 3 OR  item.is_published IS TRUE )'])
