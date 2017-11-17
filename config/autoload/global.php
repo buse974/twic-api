a0e63a51a2c8030f986bfe857a6575d88baace4e
Paul BOUSSEKEY <pboussekey@thestudnet.com>
2017-11-17 15:42:04 +0100

    Comment rtserver authentication

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -14,7 +14,7 @@
 
 return [
     'version' => "2.0.4",
-    'build-commit' => 945,
+    'build-commit' => 946,
     'app-conf' => [
         'cache' => 'storage_memcached',
         'secret_key' => 'toto',
@@ -144,9 +144,9 @@ return [
             'adapter' => [
                 'name' => 'memcached',
                 'options' => [
-                    'namespace' => 'LMS945',
+                    'namespace' => 'LMS946',
                     'lib_options' => [
-                        ['option' => Memcached::OPT_PREFIX_KEY, 'value' => 'LMS945'],
+                        ['option' => Memcached::OPT_PREFIX_KEY, 'value' => 'LMS946'],
                  /*       ['option' => Memcached::OPT_LIBKETAMA_COMPATIBLE, 'value' => true],
                         ['option' => Memcached::OPT_SERIALIZER, 'value' => Memcached::SERIALIZER_IGBINARY],
                         ['option' => Memcached::OPT_DISTRIBUTION, 'value' => Memcached::DISTRIBUTION_CONSISTENT],*/
